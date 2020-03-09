3c6d91d01b8c12c28be185864e201b4e6bb5432a
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-09 14:57:10 +0000

    Wider iam profile

--- resources.tf
+++ resources.tf
@@ -106,3 +106,111 @@ resource "aws_s3_bucket_policy" "published_bucket_https_only" {
   bucket = aws_s3_bucket.published.id
   policy = data.aws_iam_policy_document.published_bucket_https_only.json
 }
+
+resource "aws_iam_role" "dataset_generator" {
+  name               = "dataset_generator"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+  tags               = local.tags
+}
+
+resource "aws_iam_instance_profile" "dataset_generator" {
+  name = "dataset_generator"
+  role = aws_iam_role.dataset_generator.id
+}
+
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+#        Attach AWS policies
+resource "aws_iam_role_policy_attachment" "emr_for_ec2_attachment" {
+  role       = aws_iam_role.dataset_generator.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceforEC2Role"
+
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_for_ssm_attachment" {
+  role       = aws_iam_role.dataset_generator.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
+
+}
+
+#        Create and attach custom policy
+data "aws_iam_policy_document" "write_s3_dev" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:GetObject*",
+      "s3:DeleteObject*",
+      "s3:PutObject*",
+    ]
+
+    resources = [
+      "arn:aws:s3:::*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    resources = [
+      "arn:aws:kms:::*",
+    ]
+  }
+
+  statement {
+    sid    = "AllowUseDefaultEbsCmk"
+    effect = "Allow"
+
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    # resources = [data.terraform_remote_state.security-tools.ebs_cmk.arn]
+  }
+}
+
+resource "aws_iam_policy" "write_s3_dev" {
+  name        = "DatasetGeneratorWriteS3"
+  description = "Allow Dataset Generator clusters to write to S3 buckets"
+  policy      = data.aws_iam_policy_document.write_s3_dev.json
+}
+
+resource "aws_iam_role_policy_attachment" "emr_write_s3_dev" {
+  role       = aws_iam_role.dataset_generator.name
+  policy_arn = aws_iam_policy.write_s3_dev.arn
+}
