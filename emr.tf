f1306e6743f90a5118ab29b018b01a403e113dc6
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-03-11 12:32:19 +0000

    DW-3603-Create-analytical-dataset-generation-emr-cluster

--- emr.tf
+++ emr.tf
@@ -1,18 +1,23 @@
 resource "aws_emr_cluster" "cluster" {
-  name                              = var.emr_cluster_name
+  name                              = local.emr_cluster_name
   release_label                     = var.emr_release_label
   applications                      = var.emr_applications
   termination_protection            = var.termination_protection
   keep_job_flow_alive_when_no_steps = var.keep_flow_alive
+  //TODO The below is need for transparent encryption/decryption when insecure DKS is set up
   //security_configuration            = aws_emr_security_configuration.emrfs_em.id
   service_role                      = aws_iam_role.analytical_dataset_generator.arn
-  //log_uri                           = format("s3n://%s/logs/", aws_s3_bucket.emr.id)
+  //TODO which is the right bucket for logs , publish bucket??
+  log_uri                           = format("s3n://%s/logs/", aws_s3_bucket.published.id)
   ebs_root_volume_size              = local.ebs_root_volume_size
+  //TODO Does this cluster autoscales?
   //autoscaling_role                  = aws_iam_role.emr_autoscaling_role.arn
-  tags                              = merge({ "Name" = var.emr_cluster_name, "SSMEnabled" = "True" }, local.common_tags)
+  tags                              = merge({ "Name" = local.emr_cluster_name, "SSMEnabled" = "True" }, local.common_tags)
+  //TODO the below needs to be replaced with DW-EMR-AMI
   custom_ami_id                     = "ami-0c6b1df662f3c55fc"
 
   ec2_attributes {
+    subnet_id                         = "subnet-001686d576f798979"
     additional_master_security_groups = aws_security_group.analytical_dataset_generation.id
     additional_slave_security_groups  = aws_security_group.analytical_dataset_generation.id
     instance_profile                  = aws_iam_instance_profile.analytical_dataset_generator.arn
@@ -22,8 +27,6 @@ resource "aws_emr_cluster" "cluster" {
     name           = "MASTER"
     instance_count = 1
     instance_type  = local.master_instance_type
-    # bid_price      = local.master_bid_price
-
     ebs_config {
       size                 = local.ebs_config_size
       type                 = local.ebs_config_type
@@ -36,8 +39,6 @@ resource "aws_emr_cluster" "cluster" {
     name           = "CORE"
     instance_count = local.core_instance_count
     instance_type  = local.core_instance_type
-    # bid_price      = local.core_bid_price
-
     ebs_config {
       size                 = local.ebs_config_size
       type                 = local.ebs_config_type
@@ -50,48 +51,51 @@ resource "aws_emr_cluster" "cluster" {
   configurations_json = templatefile(format("%s/configuration.json", path.module), {
     logs_bucket_path     = format("s3://%s/logs", aws_s3_bucket.published.id)
     data_bucket_path     = format("s3://%s/data", aws_s3_bucket.published.id)
-    //hbase_root_path     = format("s3://%s/hbase", aws_s3_bucket.emr.id)
+    //TODO this path needs to be taken from the output of aws-ingestion
+    hbase_root_path     = data.terraform_remote_state.ingest.outputs.s3_buckets.input_bucket+"/business-data/single-topic-per-table-hbase"
     hive_external_path   = format("s3://%s/hive/external", aws_s3_bucket.published.id)
-    //notebook_bucket_path = format("%s/data", aws_s3_bucket.emr.id)
     proxy_host           = local.internet_proxy["dns_name"]
   })
 
-  // TODO connect to ECR
-  //  bootstrap_action {
-  //    name = "docker-setup"
-  //    path = format("s3://%s/%s", aws_s3_bucket.emr.id, aws_s3_bucket_object.docker_setup_sh.key)
-  //  }
-
+ //TODO The below has to be done when DKS is set up
  /* bootstrap_action {
     name = "get-dks-cert"
     path = format("s3://%s/%s", aws_s3_bucket.emr.id, aws_s3_bucket_object.get_dks_cert_sh.key)
   }*/
 
-  /*step {
-    name              = "emr-setup"
+  step {
+    name              = "copy-hbase-configuration"
     action_on_failure = "TERMINATE_CLUSTER"
     hadoop_jar_step {
-      jar = "s3://eu-west-2.elasticmapreduce/libs/script-runner/script-runner.jar"
+      jar = "command-runner.jar"
       args = [
-        format("s3://%s/%s", aws_s3_bucket.emr.id, aws_s3_bucket_object.emr_setup_sh.key)
+        "bash",
+        "-c",
+        "sudo cp /etc/hbase/conf/hbase-site.xml /etc/spark/conf/"
       ]
     }
-  }*/
+  }
 
- /* step {
-    name              = "livy-client-conf"
-    action_on_failure = "TERMINATE_CLUSTER"
-    hadoop_jar_step {
-      jar = "s3://eu-west-2.elasticmapreduce/libs/script-runner/script-runner.jar"
-      args = [
-        format("s3://%s/%s", aws_s3_bucket.emr.id, aws_s3_bucket_object.livy_client_conf_sh.key)
-      ]
-    }
-  }*/
+  step {
+      name              = "emr-setup"
+      action_on_failure = "CONTINUE"
+      hadoop_jar_step {
+        jar = "command-runner.jar"
+        args = [
+          "spark-submit",
+          format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.generate-analytical-dataset-script.key),
+          "--deploy-mode",
+          "cluster",
+          "--master",
+          "yarn",
+          "--conf",
+          "spark.yarn.submit.waitAppCompletion=true"
+        ]
+      }
+  }
 
   depends_on = [
-    //aws_s3_bucket_object.get_dks_cert_sh,
-    //aws_s3_bucket_object.livy_client_conf_sh,
+    aws_s3_bucket_object.generate-analytical-dataset-script
   ]
 
   lifecycle {
