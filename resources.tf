43d4badf1c0b4332b8a99fa9c91edfa9597bcdf4
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2020-03-19 10:02:35 +0000

    DW-3639-Create-ACM-PCA-Certificate

--- resources.tf
+++ resources.tf
@@ -273,6 +273,8 @@ output "analytical_dataset_generation" {
   }
 }
 
+
+
 resource "aws_acm_certificate" "analytical_dataset_generation" {
   certificate_authority_arn = data.terraform_remote_state.aws_certificate_authority.outputs.cert_authority.arn
   domain_name               = "analytical_dataset_generation.${local.env_prefix[local.environment]}dataworks.dwp.gov.uk"
