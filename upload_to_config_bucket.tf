1bc466e52c5320603192c16195777f2311a150c3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2020-03-20 14:17:38 +0000

    Fixed some formatting

--- upload_to_config_bucket.tf
+++ upload_to_config_bucket.tf
@@ -14,11 +14,11 @@ data "template_file" "analytical_dataset_generation_script" {
 resource "aws_s3_bucket_object" "analytical_dataset_generator_cluster_payload" {
   bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
   key        = "component/analytical-dataset-generation/analytical-dataset-generator-cluster-payload.json"
-  content    = data.template_file.analytical-dataset-generator-cluster-payload.rendered
+  content    = data.template_file.analytical_dataset_generator_cluster_payload.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
-data "template_file" "analytical_dataset_generator_cluster-payload" {
+data "template_file" "analytical_dataset_generator_cluster_payload" {
   template = file(format("%s/payload.json", path.module))
   vars = {
   }
