dc800dece2420bdf849f95d95b86d303c2eb31aa
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-03-20 13:31:12 +0000

    Trigger synthetic data ingestion

--- upload_to_config_bucket.tf
+++ upload_to_config_bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket_object" "generate-analytical-dataset-script" {
-  bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
-  key     = "component/analytical-dataset-generation/generate-analytical-dataset.py"
-  content = data.template_file.analytical_dataset_generation_script.rendered
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/analytical-dataset-generation/generate-analytical-dataset.py"
+  content    = data.template_file.analytical_dataset_generation_script.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
