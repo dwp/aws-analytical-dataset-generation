e588760f4590b27b7c9291a9b477ba5bcfb19221
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-03-27 11:48:07 +0000

    acceptable ip

--- upload_to_config_bucket.tf
+++ upload_to_config_bucket.tf
@@ -23,3 +23,22 @@ data "template_file" "analytical_dataset_generator_cluster_payload" {
   vars = {
   }
 }
+resource "aws_s3_bucket_object" "emr_setup_sh" {
+  bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
+  key     = "component/emr-setup.sh"
+  content = data.template_file.emr_setup_sh.rendered
+}
+
+data "template_file" "emr_setup_sh" {
+  template = file(format("%s/emr-setup.sh", path.module))
+  vars = {
+    aws_default_region = "eu-west-2"
+    full_proxy         = local.internet_proxy["http_address"]
+    full_no_proxy      = "169.254.169.254|*.s3.eu-west-2.amazonaws.com|s3.eu-west-2.amazonaws.com|sns.eu-west-2.amazonaws.com|sqs.eu-west-2.amazonaws.com|eu-west-2.queue.amazonaws.com|glue.eu-west-2.amazonaws.com|sts.eu-west-2.amazonaws.com|*.eu-west-2.compute.internal|dynamodb.eu-west-2.amazonaws.com"
+    acm_cert_arn       = local.cert_authority_arn
+    private_key_alias  = "private_key"
+    truststore_aliases = "ca_cert"
+    truststore_certs   = "s3://${local.env_certificate_bucket}/ca_certificates/dataworks/ca.pem,s3://dw-management-dev-public-certificates/ca_certificates/dataworks/ca.pem"
+    dks_endpoint       = local.dks_endpoint
+  }
+}
