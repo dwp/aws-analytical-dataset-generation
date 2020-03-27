fbf5f9e8ca3d6a12b78977b61be1d258066da4d1
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-03-27 11:46:38 +0000

    cluster with new cert helper 0.28

---
+++ emr-setup.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -x
+set -o pipefail
+
+# Only execute below here on an EMR Master node
+if [[ $(grep "isMaster" /mnt/var/lib/info/instance.json | grep true) ]]; then
+    echo "I am a Master"
+else
+    echo "I am a Slave, exiting"
+    exit 0
+fi
+
+echo -n "Running as: "
+whoami
+
+export AWS_DEFAULT_REGION=${aws_default_region}
+
+FULL_PROXY="${full_proxy}"
+FULL_NO_PROXY="${full_no_proxy}"
+export http_proxy="$FULL_PROXY"
+export HTTP_PROXY="$FULL_PROXY"
+export https_proxy="$FULL_PROXY"
+export HTTPS_PROXY="$FULL_PROXY"
+export no_proxy="$FULL_NO_PROXY"
+export NO_PROXY="$FULL_NO_PROXY"
+
+export ACM_KEY_PASSWORD=$(uuidgen -r)
+
+sudo mkdir -p /opt/emr
+sudo chown hadoop:hadoop /opt/emr
+touch /opt/emr/dks.properties
+cat >> /opt/emr/dks.properties <<EOF
+spark.ssl.fs.enabled=true
+data.key.service.url=${dks_endpoint}
+EOF
+
+/usr/local/bin/acm-cert-retriever \
+    --acm-cert-arn "${acm_cert_arn}" \
+    --acm-key-passphrase "$ACM_KEY_PASSWORD" \
+    --private-key-alias "${private_key_alias}" \
+    --truststore-aliases "${truststore_aliases}" \
+    --truststore-certs "${truststore_certs}"
