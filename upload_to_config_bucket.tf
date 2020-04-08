e3cf9e30d43aecc6d2c18fbb3def41f5040c5e5e
UdayChokkam <udayjavauk@gmail.com>
2020-04-08 16:01:38 +0100

    Creation of hive tables in glue database (#32)
    * Creation of hive tables in glue database

--- upload_to_config_bucket.tf
+++ upload_to_config_bucket.tf
@@ -39,7 +39,8 @@ data "template_file" "emr_setup_sh" {
     acm_cert_arn       = data.aws_acm_certificate.htme.arn
     private_key_alias  = "private_key"
     truststore_aliases = join(",", var.truststore_aliases)
-    truststore_certs   = "s3://${local.env_certificate_bucket}/ca_certificates/dataworks/ca.pem,s3://dw-management-dev-public-certificates/ca_certificates/dataworks/ca.pem,s3://${data.terraform_remote_state.mgmt_ca.outputs.public_cert_bucket.id}/ca_certificates/dataworks/root_ca.pem"
+    truststore_certs   = "s3://${local.env_certificate_bucket}/ca_certificates/dataworks/ca.pem,s3://dw-management-dev-public-certificates/ca_certificates/dataworks/ca.pem,s3://${data.terraform_remote_state.mgmt_ca.outputs.public_cert_bucket.id}/ca_certificates/dataworks/root_ca.pem",
+    hive-scripts-path  = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.create-hive-tables.key)
 
 
     dks_endpoint = local.dks_endpoint
@@ -49,4 +50,18 @@ data "template_file" "emr_setup_sh" {
 // TODO Ticket created to replace this https://projects.ucd.gpn.gov.uk/browse/DW-3765
 data "aws_acm_certificate" "htme" {
   domain = "htme.${local.root_dns_name[local.environment]}"
+}
+
+resource "aws_s3_bucket_object" "create-hive-tables" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/create-hive-tables.py"
+  content    = data.template_file.create-hive-tables.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+}
+
+data "template_file" "create-hive-tables" {
+  template = file(format("%s/hive-tables-creation.py", path.module))
+  vars = {
+    bucket     = aws_s3_bucket.published.id
+  }
 }
