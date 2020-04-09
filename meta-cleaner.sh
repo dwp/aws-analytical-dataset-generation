677e928ef381d2b24c7caed8fbacb9931822cf83
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-04-09 16:29:36 +0100

    added emrfs sync, to align with s3

--- meta-cleaner.sh
+++ meta-cleaner.sh
@@ -4,5 +4,6 @@ export CLUSTER_ID=$(cat /mnt/var/lib/info/job-flow.json|jq -r ".jobFlowId")
 export HBASE_META=${hbase_meta}
 echo "Removing all files from" $HBASE_META$CLUSTER_ID
 aws s3 rm --recursive $HBASE_META$CLUSTER_ID
-aws s3 rm --recursive $HBASE_META$CLUSTER_ID
-aws s3 rm  $HBASE_META$CLUSTER_ID'$folder$'
+aws s3 rm  $HBASE_META$CLUSTER_ID'_$folder$'
+
+emrfs sync
