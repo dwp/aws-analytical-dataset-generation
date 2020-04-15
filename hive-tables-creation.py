25ce86e0fcfaab213b8837cb5a0c1ccf2696a60f
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2020-04-15 13:59:46 +0100

    Dw 3759 clean up (#35)
    * cleaning s3 folder from metadata
    
    * also deleting the meta_CLUSTER_ID_$
    
    * added emrfs sync, to align with s3
    
    * added emrfs sync, to align with s3
    
    * adding s3 consistency and dynamo clean-up
    
    * adding dynamo cleanup
    
    * commented out sync command
    
    * commented out cleanup step

--- hive-tables-creation.py
+++ hive-tables-creation.py
@@ -1,39 +1,33 @@
 import boto3
 
-client = boto3.client('glue')
+client = boto3.client("glue")
 client.create_table(
-    DatabaseName='analytical_dataset_generation',
+    DatabaseName="analytical_dataset_generation",
     TableInput={
-        'Name': 'core_contract_hbase',
-        'Description': 'Hive table to access hbase table core:contract',
-        'StorageDescriptor': {
-            'Columns': [
-                {
-                    'Name': 'rowkey',
-                    'Type': 'string'
-                },
-                {
-                    'Name': 'data',
-                    'Type': 'string'
-                }
+        "Name": "core_contract_hbase",
+        "Description": "Hive table to access hbase table core:contract",
+        "StorageDescriptor": {
+            "Columns": [
+                {"Name": "rowkey", "Type": "string"},
+                {"Name": "data", "Type": "string"},
             ],
-            'Location': 's3://${bucket}/analytical-dataset/hive/external/core_contract_hbase',
-            'Compressed': False,
-            'NumberOfBuckets': -1,
-            'SerdeInfo': {
-                'Name': 'string',
-                'SerializationLibrary': 'org.apache.hadoop.hive.hbase.HBaseSerDe',
-                'Parameters': {
-                    'hbase.columns.mapping': ':key,cf:record',
-                    'serialization.format': '1'
-                }
+            "Location": "s3://${bucket}/analytical-dataset/hive/external/core_contract_hbase",
+            "Compressed": False,
+            "NumberOfBuckets": -1,
+            "SerdeInfo": {
+                "Name": "string",
+                "SerializationLibrary": "org.apache.hadoop.hive.hbase.HBaseSerDe",
+                "Parameters": {
+                    "hbase.columns.mapping": ":key,cf:record",
+                    "serialization.format": "1",
+                },
             },
         },
-        'TableType': 'EXTERNAL_TABLE',
-        'Parameters': {
-            'hbase.table.name': 'core:contract',
-            'storage_handler':'org.apache.hadoop.hive.hbase.HBaseStorageHandler',
-            'EXTERNAL': 'True'
-        }
-    }
+        "TableType": "EXTERNAL_TABLE",
+        "Parameters": {
+            "hbase.table.name": "core:contract",
+            "storage_handler": "org.apache.hadoop.hive.hbase.HBaseStorageHandler",
+            "EXTERNAL": "True",
+        },
+    },
 )
