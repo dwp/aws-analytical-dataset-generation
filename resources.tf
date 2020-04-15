18511ea48f15139a6b77a3ced565d863ac7e4d8b
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-04-15 11:19:27 +0100

    New  certificate for analytical-dataset-generator

--- resources.tf
+++ resources.tf
@@ -277,8 +277,8 @@ output "analytical_dataset_generation" {
 
 
 
-resource "aws_acm_certificate" "analytical_dataset_generation" {
-  certificate_authority_arn = data.terraform_remote_state.aws_certificate_authority.outputs.cert_authority.arn
+resource "aws_acm_certificate" "analytical-dataset-generator" {
+  certificate_authority_arn = data.terraform_remote_state.aws_certificate_authority.outputs.root_ca.arn
   domain_name               = "analytical-dataset-generator.${local.env_prefix[local.environment]}${local.dataworks_domain_name}"
 
   options {
