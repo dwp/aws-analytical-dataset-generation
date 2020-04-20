d88d866db1603fcb44334e3067b2face7d9ea7be
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-04-20 11:26:22 +0100

    Adding EMR launcher terraform

---
+++ emr-launcher.tf
@@ -0,0 +1,102 @@
+variable "adg_emr_launcher_zip" {
+  type = map(string)
+
+  default = {
+    base_path = ""
+    version   = ""
+  }
+}
+
+resource "aws_lambda_function" "adg_emr_launcher" {
+  filename      = "${var.adg_emr_launcher_zip["base_path"]}/emr-launcher-${var.adg_emr_launcher_zip["version"]}.zip"
+  function_name = "adg_emr_launcher"
+  role          = aws_iam_role.adg_emr_launcher_lambda_role.arn
+  handler       = "emr_launcher.handler"
+  runtime       = "python3.7"
+  source_code_hash = filebase64sha256(
+  format(
+  "%s/emr-launcher-%s.zip",
+  var.adg_emr_launcher_zip["base_path"],
+  var.adg_emr_launcher_zip["version"]
+  )
+  )
+  publish = false
+  timeout = 60
+
+  environment {
+    variables = {
+      EMR_LAUNCHER_CONFIG_DIR = "docs/examples"
+      EMR_LAUNCHER_LOG_LEVEL = "debug"
+    }
+  }
+}
+
+resource "aws_iam_role" "adg_emr_launcher_lambda_role" {
+  name               = "adg_emr_launcher_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.adg_emr_launcher_assume_policy.json
+}
+
+data "aws_iam_policy_document" "adg_emr_launcher_assume_policy" {
+  statement {
+    sid    = "AdgEmrLauncherLambdaAssumeRolePolicy"
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "adg_emr_launcher_read_secret_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:*",
+      "elasticmapreduce:*"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "adg_emr_launcher_pass_role_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "iam:PassRole"
+    ]
+    resources = [
+      "arn:aws:iam::*:role/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "adg_emr_launcher_read_secrets_policy" {
+  name        = "AdgReadSecrets"
+  description = "Allow ADG to read secrets"
+  policy      = data.aws_iam_policy_document.adg_emr_launcher_read_secret_policy.json
+}
+
+resource "aws_iam_policy" "adg_emr_launcher_pass_role_policy" {
+  name        = "AdgPassRole"
+  description = "Allow ADG to pass role"
+  policy      = data.aws_iam_policy_document.adg_emr_launcher_pass_role_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "adg_emr_launcher_pass_role_attachment" {
+  role       = aws_iam_role.adg_emr_launcher_lambda_role.name
+  policy_arn = aws_iam_policy.adg_emr_launcher_pass_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "adg_emr_launcher_read_secrets_attachment" {
+  role       = aws_iam_role.adg_emr_launcher_lambda_role.name
+  policy_arn = aws_iam_policy.adg_emr_launcher_read_secrets_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "adg_emr_launcher_policy_execution" {
+  role       = aws_iam_role.adg_emr_launcher_lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
