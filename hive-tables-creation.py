4a5a0bd1a395081fe74709c0f7a4adbd5fcdf383
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-04-22 16:10:35 +0100

    put actions under 1 loop

--- hive-tables-creation.py
+++ hive-tables-creation.py
@@ -1,52 +1,48 @@
 import boto3
 import csv
 
-collections = []
+client = boto3.client("glue")
 DatabaseName = "analytical_dataset_generation"
 with open("collections.csv", "rb") as csvfile:
-    reader = csv.reader(csvfile, delimiter=",")
-    for row in reader:
-        collections.append(row[0].replace(":", "_"))
+    collections = csv.reader(csvfile, delimiter=",")
+    for collection in collections:
+        collection = collection[0]
+        collection_hbase = collection.replace(":", "_") + "_hbase"
+        # TODO make connection to happybase and check if table exists before creating it
 
-client = boto3.client("glue")
+        try:
+            client.delete_table(DatabaseName=DatabaseName, Name=collection_hbase)
+        except client.exceptions.EntityNotFoundException as e:
+            print(e)
 
-
-for collection in collections:
-    collection_hbase = collection + "_hbase"
-    # TODO make connection to happybase and check if table exists before creating it
-    try:
-        client.delete_table(DatabaseName=DatabaseName, Name=collection_hbase)
-    except Exception as e:
-        print(e)
-
-    client.create_table(
-        DatabaseName=DatabaseName,
-        TableInput={
-            "Name": collection_hbase,
-            "Description": "Hive table to access hbase table " + collection,
-            "StorageDescriptor": {
-                "Columns": [
-                    {"Name": "rowkey", "Type": "string"},
-                    {"Name": "data", "Type": "string"},
-                ],
-                "Location": "s3://${bucket}/analytical-dataset/hive/external/"
-                + collection_hbase,
-                "Compressed": False,
-                "NumberOfBuckets": -1,
-                "SerdeInfo": {
-                    "Name": "string",
-                    "SerializationLibrary": "org.apache.hadoop.hive.hbase.HBaseSerDe",
-                    "Parameters": {
-                        "hbase.columns.mapping": ":key,cf:record",
-                        "serialization.format": "1",
+        client.create_table(
+            DatabaseName=DatabaseName,
+            TableInput={
+                "Name": collection_hbase,
+                "Description": "Hive table to access hbase table " + collection,
+                "StorageDescriptor": {
+                    "Columns": [
+                        {"Name": "rowkey", "Type": "string"},
+                        {"Name": "data", "Type": "string"},
+                    ],
+                    "Location": "s3://${bucket}/analytical-dataset/hive/external/"
+                    + collection_hbase,
+                    "Compressed": False,
+                    "NumberOfBuckets": -1,
+                    "SerdeInfo": {
+                        "Name": "string",
+                        "SerializationLibrary": "org.apache.hadoop.hive.hbase.HBaseSerDe",
+                        "Parameters": {
+                            "hbase.columns.mapping": ":key,cf:record",
+                            "serialization.format": "1",
+                        },
                     },
                 },
+                "TableType": "EXTERNAL_TABLE",
+                "Parameters": {
+                    "hbase.table.name": collection,
+                    "storage_handler": "org.apache.hadoop.hive.hbase.HBaseStorageHandler",
+                    "EXTERNAL": "True",
+                },
             },
-            "TableType": "EXTERNAL_TABLE",
-            "Parameters": {
-                "hbase.table.name": collection.replace("_", ":"),
-                "storage_handler": "org.apache.hadoop.hive.hbase.HBaseStorageHandler",
-                "EXTERNAL": "True",
-            },
-        },
-    )
+        )
