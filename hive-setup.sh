14fe3ddcfe1f269ca209d21c67db851568773db4
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-04-23 17:57:04 +0100

    installing happybase, problems with boto3

--- hive-setup.sh
+++ hive-setup.sh
@@ -2,4 +2,4 @@
 aws s3 cp "${hive-scripts-path}"  .
 aws s3 cp "${collections_list}"  .
 
-/usr/bin/python3 create-hive-tables.py
+/usr/bin/python3.6 create-hive-tables.py
--- installer.sh
+++ installer.sh
@@ -1,2 +1,17 @@
+FULL_PROXY="${full_proxy}"
+FULL_NO_PROXY="${full_no_proxy}"
+export http_proxy="$FULL_PROXY"
+export HTTP_PROXY="$FULL_PROXY"
+export https_proxy="$FULL_PROXY"
+export HTTPS_PROXY="$FULL_PROXY"
+export no_proxy="$FULL_NO_PROXY"
+export NO_PROXY="$FULL_NO_PROXY"
+
+
+sudo chown hadoop:hadoop /usr/lib/python3.6/dist-packages
+sudo chown hadoop:hadoop /usr/lib64/python3.6/dist-packages
+sudo chown hadoop:hadoop /usr/bin
+sudo chown hadoop:hadoop /usr/bin/rst2html.py
+
 /usr/bin/pip-3.6 install boto3
 /usr/bin/pip-3.6 install happybase
