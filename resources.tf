052d840bc4bbc7691b2ef29889fbe1a9ec80db47
Gordon Adam <gordon.adam@dwp.gov.uk>
2020-04-29 14:49:24 +0100

    Creating secret in terraform instad of just sourcing

--- resources.tf
+++ resources.tf
@@ -325,7 +325,7 @@ resource "aws_iam_role_policy_attachment" "emr_analytical_dataset_acm" {
   policy_arn = aws_iam_policy.analytical_dataset_acm.arn
 }
 
-data "aws_secretsmanager_secret" "adg_secret" {
+resource "aws_secretsmanager_secret" "adg_secret" {
   name = "ADG-Payload"
 }
 
@@ -338,7 +338,7 @@ data "aws_iam_policy_document" "analytical_dataset_secretsmanager" {
     ]
 
     resources = [
-      "${data.aws_secretsmanager_secret.adg_secret.arn}"
+      aws_secretsmanager_secret.adg_secret.arn
     ]
   }
 }
