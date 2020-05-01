237314c030feea86f3d5a566932a508943ee7dd4
Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>
2020-05-01 14:38:33 +0100

    DW-3936 - Fix EMR AMI and change Secret back to data
    Signed-off-by: Benjamin Sherwood <benjaminsherwood@digital.uc.dwp.gov.uk>

--- resources.tf
+++ resources.tf
@@ -331,7 +331,7 @@ resource "aws_iam_role_policy_attachment" "emr_analytical_dataset_acm" {
   policy_arn = aws_iam_policy.analytical_dataset_acm.arn
 }
 
-resource "aws_secretsmanager_secret" "adg_secret" {
+data "aws_secretsmanager_secret" "adg_secret" {
   name = "ADG-Secret"
 }
 
