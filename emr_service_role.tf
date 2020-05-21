c1047c2c452e56a281814ac0fd65ad65651d32f8
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-05-21 14:34:18 +0100

    EMR Service doesn't need to decrypt EBS volumes

--- emr_service_role.tf
+++ emr_service_role.tf
@@ -21,33 +21,3 @@ resource "aws_iam_role_policy_attachment" "emr_attachment" {
   role       = aws_iam_role.adg_emr_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
 }
-
-data "aws_iam_policy_document" "analytical_dataset_ebs_cmk" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "kms:Encrypt",
-      "kms:Decrypt",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:DescribeKey",
-    ]
-
-    resources = [aws_kms_key.adg_ebs_cmk.arn]
-  }
-
-  statement {
-    effect = "Allow"
-
-    actions = ["kms:CreateGrant"]
-
-    resources = [aws_kms_key.adg_ebs_cmk.arn]
-
-    condition {
-      test     = "Bool"
-      variable = "kms:GrantIsForAWSResource"
-      values   = ["true"]
-    }
-  }
-}
