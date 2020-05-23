50f2b54456a2a3f75b9be641853be8d88917cbe2
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-05-23 23:33:21 +0100

    Move steps S3 objects to their own file

--- steps.tf
+++ steps.tf
@@ -1,14 +1,15 @@
-resource "aws_s3_bucket_object" "generate_analytical_dataset_script" {
-  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "component/analytical-dataset-generation/generate_analytical_dataset.py"
-  content    = data.template_file.analytical_dataset_generation_script.rendered
-  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+data "template_file" "create-hive-tables" {
+  template = file("steps/create-hive-tables.py")
+  vars = {
+    bucket      = aws_s3_bucket.published.id
+    secret_name = local.secret_name
+  }
 }
 
-resource "aws_s3_bucket_object" "logger_script" {
+resource "aws_s3_bucket_object" "create-hive-tables" {
   bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "component/analytical-dataset-generation/logger.py"
-  content    = data.template_file.logger.rendered
+  key        = "component/analytical-dataset-generation/create-hive-tables.py"
+  content    = data.template_file.create-hive-tables.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
 
@@ -24,25 +25,11 @@ data "template_file" "analytical_dataset_generation_script" {
   }
 }
 
-resource "aws_s3_bucket_object" "create-hive-tables" {
+resource "aws_s3_bucket_object" "generate_analytical_dataset_script" {
   bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "component/analytical-dataset-generation/create-hive-tables.py"
-  content    = data.template_file.create-hive-tables.rendered
+  key        = "component/analytical-dataset-generation/generate_analytical_dataset.py"
+  content    = data.template_file.analytical_dataset_generation_script.rendered
   kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
-}
-
-data "template_file" "create-hive-tables" {
-  template = file("steps/create-hive-tables.py")
-  vars = {
-    bucket      = aws_s3_bucket.published.id
-    secret_name = local.secret_name
-  }
-}
-
-resource "aws_s3_bucket_object" "hive_setup_sh" {
-  bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
-  key     = "component/analytical-dataset-generation/hive-setup.sh"
-  content = data.template_file.hive_setup_sh.rendered
 }
 
 data "template_file" "hive_setup_sh" {
@@ -52,15 +39,21 @@ data "template_file" "hive_setup_sh" {
   }
 }
 
-resource "aws_s3_bucket_object" "logger" {
-  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
-  key        = "component/analytical-dataset-generation/logger.py"
-  content    = data.template_file.logger.rendered
-  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
+resource "aws_s3_bucket_object" "hive_setup_sh" {
+  bucket  = data.terraform_remote_state.common.outputs.config_bucket.id
+  key     = "component/analytical-dataset-generation/hive-setup.sh"
+  content = data.template_file.hive_setup_sh.rendered
 }
 
 data "template_file" "logger" {
   template = file("steps/logger.py")
   vars = {
   }
+}
+
+resource "aws_s3_bucket_object" "logger" {
+  bucket     = data.terraform_remote_state.common.outputs.config_bucket.id
+  key        = "component/analytical-dataset-generation/logger.py"
+  content    = data.template_file.logger.rendered
+  kms_key_id = data.terraform_remote_state.common.outputs.config_bucket_cmk.arn
 }
