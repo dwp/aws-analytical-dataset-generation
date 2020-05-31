1e40f9d9d7a3c93f6ddf1185bba9894d524149b9
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-05-31 22:26:59 +0100

    Fix typo

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket_object" "emr_setup_sh" {
       s3_common_logging_shell = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, data.terraform_remote_state.common.outputs.application_logging_common_file.s3_id)
       s3_logging_shell        = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.logging_script.key)
       aws_default_region      = "eu-west-2"
-      http_proxt              = data.terraform_remote_state.internet_egress.outputs.internet_proxy.http_address
+      http_proxy              = data.terraform_remote_state.internet_egress.outputs.internet_proxy.http_address
       no_proxy                = local.no_proxy
       acm_cert_arn            = aws_acm_certificate.analytical-dataset-generator.arn
       private_key_alias       = "adg"
