0840773f2c2fb8495fea41d0a9cb70e08d24f5f9
Levan Kirvalidze <levanik@live.com>
2020-06-25 15:27:10 +0100

    removing new line

--- steps/hive-setup.sh
+++ steps/hive-setup.sh
@@ -35,7 +35,6 @@ log_wrapper_message "Creating hive tables"
 
 /usr/bin/python3.6 /opt/emr/create-hive-tables.py >> /var/log/adg/create-hive-tables.log 2>&1
 
-
 log_wrapper_message "Completed the hive-setup.sh step of the EMR Cluster"
 
 ) >> /var/log/adg/nohup.log 2>&1
