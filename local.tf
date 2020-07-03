c5297c69f20044e6382ded28adb48da1db7b2034
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-07-03 13:39:01 +0100

    Shorten the no_proxy variable for Spark parameter length constraints

--- local.tf
+++ local.tf
@@ -71,7 +71,7 @@ locals {
   }
 
   amazon_region_domain = "${data.aws_region.current.name}.amazonaws.com"
-  endpoint_services    = ["autoscaling", "dynamodb", "ec2", "ec2messages", "ecr.dkr", "glue", "kms", "logs", "monitoring", ".s3", "s3", "secretsmanager", "sns", "sqs", "ssm", "ssmmessages"]
+  endpoint_services    = ["dynamodb", "ec2", "ec2messages", "glue", "kms", "logs", "monitoring", ".s3", "s3", "secretsmanager", "ssm", "ssmmessages"]
   no_proxy             = "169.254.169.254,${join(",", formatlist("%s.%s", local.endpoint_services, local.amazon_region_domain))}"
 
   ebs_emrfs_em = {
