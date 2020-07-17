be00910c2e56976499d4ab7bcb1986f1b6efdb97
MattToczek <matthew.toczek@dmwgroup.co.uk>
2020-07-17 15:55:03 +0100

    adding permissions for bucket in non_pii

--- published_bucket.tf
+++ published_bucket.tf
@@ -311,7 +311,7 @@ data "aws_iam_policy_document" "analytical_dataset_crown_read_only_non_pii" {
     ]
 
     resources = [
-      aws_s3_bucket.published.arn,
+      "${aws_s3_bucket.published.arn}/analytical-dataset/*",
     ]
 
     condition {
