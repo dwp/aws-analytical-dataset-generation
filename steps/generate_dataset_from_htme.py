3dc14c9041144f3812d4047ab5bcde97ce847fb1
Sandeep Gopishetty <sandeep.gopishetty@Sandeeps-MacBook-Pro.local>
2020-08-27 09:49:56 +0100

    Added tests and refactored main script to make it testabale and improve code quality

--- steps/generate_dataset_from_htme.py
+++ steps/generate_dataset_from_htme.py
@@ -1,21 +1,21 @@
 import ast
 import base64
-import boto3
+import concurrent.futures
+import itertools
 import logging
 import os
 import re
-import requests
-import zlib
-import concurrent.futures
 import time
+import zlib
 from datetime import datetime
+from itertools import groupby
 
+import boto3
+import requests
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
-from itertools import groupby
 from logger import setup_logging
-from pyspark.sql import Row, SparkSession
-from pyspark.sql import functions as F
+from pyspark.sql import SparkSession
 
 the_logger = setup_logging(
     log_level=os.environ["ADG_LOG_LEVEL"].upper()
@@ -25,31 +25,40 @@ the_logger = setup_logging(
 )
 
 
-
-def main():
-    keys = get_list_keys_for_prefix()
+def main(spark, s3_client, s3_htme_bucket,
+         s3_prefix, secrets_collections, keys_map,
+         run_time_stamp, s3_publish_bucket, published_database_name):
+    keys = get_list_keys_for_prefix(s3_client, s3_htme_bucket, s3_prefix)
     list_of_dicts = group_keys_by_collection(keys)
-    list_of_dicts_filtered = get_collections_in_secrets (list_of_dicts)
+    list_of_dicts_filtered = get_collections_in_secrets(list_of_dicts, secrets_collections)
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(consolidate_rdd_per_collection, list_of_dicts_filtered)
+        _ = executor.map(consolidate_rdd_per_collection, list_of_dicts_filtered,
+                         itertools.repeat(secrets_collections),
+                         itertools.repeat(s3_htme_bucket),
+                         itertools.repeat(spark),
+                         itertools.repeat(keys_map),
+                         itertools.repeat(run_time_stamp),
+                         itertools.repeat(s3_publish_bucket),
+                         itertools.repeat(published_database_name))
 
-def get_collections_in_secrets(list_of_dicts):
+
+def get_collections_in_secrets(list_of_dicts, secrets_collections):
     filtered_list = []
     for collection_dict in list_of_dicts:
         for collection_name, collection_files_keys in collection_dict.items():
             if collection_name.lower() in secrets_collections:
                 filtered_list.append(collection_dict)
             else:
-                logging.error(
-                    collection_name + " is not present in the collections list "
-                )
+                logging.error("%s is not present in the collections list" % collection_name)
     return filtered_list
+
 
 def get_client(service_name):
     client = boto3.client(service_name)
     return client
 
-def get_list_keys_for_prefix():
+
+def get_list_keys_for_prefix(s3_client, s3_htme_bucket, s3_prefix):
     keys = []
     paginator = s3_client.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=s3_htme_bucket, Prefix=s3_prefix)
@@ -64,10 +73,10 @@ def get_list_keys_for_prefix():
 def group_keys_by_collection(keys):
     file_key_dict = {key.split("/")[-1]: key for key in keys}
     file_names = list(file_key_dict.keys())
-    file_pattern = "^\w+\.([\w-]+)\.([\w]+)"
+    file_pattern = r"^\w+\.([\w-]+)\.([\w]+)"
     grouped_files = []
     for pattern, group in groupby(
-        file_names, lambda x: re.match(file_pattern, x).group()
+            file_names, lambda x: re.match(file_pattern, x).group()
     ):
         grouped_files.append({pattern: list(group)})
     list_of_dicts = []
@@ -79,34 +88,36 @@ def group_keys_by_collection(keys):
         list_of_dicts.append({k: gh})
     return list_of_dicts
 
-def consolidate_rdd_per_collection(collection):
+
+def consolidate_rdd_per_collection(collection, secrets_collections, s3_client,
+                                   s3_htme_bucket, spark, keys_map, run_time_stamp,
+                                   s3_publish_bucket, published_database_name):
     for collection_name, collection_files_keys in collection.items():
-        the_logger.info("Processing collection : " + collection_name)
+        the_logger.info("Processing collection : %s" % collection_name)
         tag_value = secrets_collections[collection_name.lower()]
-        print(f"Processing Collection {collection_name}")
         start_time = time.perf_counter()
         rdd_list = []
         for collection_file_key in collection_files_keys:
-            encrypted = read_binary(
-                f"s3://{s3_htme_bucket}/{collection_file_key}"
-            )
-            add_filesize_metric(collection_name, s3_htme_bucket, collection_file_key)
-            metadata = get_metadatafor_key(collection_file_key)
+            encrypted = read_binary(spark,
+                                    f"s3://{s3_htme_bucket}/{collection_file_key}"
+                                    )
+            add_filesize_metric(collection_name, s3_client, s3_htme_bucket, collection_file_key)
+            metadata = get_metadatafor_key(collection_file_key, s3_client, s3_htme_bucket)
             ciphertext = metadata["ciphertext"]
             datakeyencryptionkeyid = metadata["datakeyencryptionkeyid"]
             iv = metadata["iv"]
             plain_text_key = get_plaintext_key_calling_dks(
-                ciphertext, datakeyencryptionkeyid
+                ciphertext, datakeyencryptionkeyid, keys_map
             )
             decrypted = encrypted.mapValues(
-                lambda val, plain_text_key = plain_text_key, iv = iv: decrypt(plain_text_key, iv, val)
+                lambda val, plain_text_key=plain_text_key, iv=iv: decrypt(plain_text_key, iv, val)
             )
             decompressed = decrypted.mapValues(decompress)
             decoded = decompressed.mapValues(decode)
             rdd_list.append(decoded)
         consolidated_rdd = spark.sparkContext.union(rdd_list)
         consolidated_rdd_mapped = consolidated_rdd.map(lambda x: x[1])
-        the_logger.info("Persisting Json : " + collection_name)
+        the_logger.info("Persisting Json : %s" % collection_name)
         json_location_prefix = "${file_location}/%s/%s/%s" % (
             run_time_stamp,
             get_collection(collection_name),
@@ -118,19 +129,20 @@ def consolidate_rdd_per_collection(collection):
         )
         persist_json(json_location, consolidated_rdd_mapped)
         the_logger.info("Applying Tags for prefix : " + json_location_prefix)
-        tag_objects(json_location_prefix, tag_value)
-    the_logger.info("Creating Hive tables for : " + collection_name)
-    create_hive_on_published(json_location, collection_name)
+        tag_objects(json_location_prefix, tag_value, s3_client, s3_publish_bucket)
+    the_logger.info("Creating Hive tables for : %s" % collection_name)
+    create_hive_on_published(spark, json_location, collection_name, published_database_name)
     end_time = time.perf_counter()
     total_time = round(end_time - start_time)
     add_metric("processing_times.csv", collection_name, str(total_time))
-    the_logger.info("Completed Processing : " + collection_name)
+    the_logger.info("Completed Processing : %s" % collection_name)
 
 
 def decode(txt):
-    return  txt.decode("utf-8")
+    return txt.decode("utf-8")
 
-def get_metadatafor_key(key):
+
+def get_metadatafor_key(key, s3_client, s3_htme_bucket):
     s3_object = s3_client.get_object(Bucket=s3_htme_bucket, Key=key)
     # print(s3_object)
     iv = s3_object["Metadata"]["iv"]
@@ -157,24 +169,20 @@ def retrieve_secrets():
     return response_dict
 
 
-def tag_objects(prefix, tag_value):
-    session = boto3.session.Session()
-    client_s3 = session.client(service_name="s3")
+def tag_objects(prefix, tag_value, s3_client, s3_publish_bucket):
     default_value = "default"
 
     if tag_value is None or tag_value == "":
         tag_value = default_value
-    for key in client_s3.list_objects(Bucket=s3_publish_bucket, Prefix=prefix)[
-        "Contents"
-    ]:
-        client_s3.put_object_tagging(
+    for key in s3_client.list_objects(Bucket=s3_publish_bucket, Prefix=prefix)["Contents"]:
+        s3_client.put_object_tagging(
             Bucket=s3_publish_bucket,
             Key=key["Key"],
             Tagging={"TagSet": [{"Key": "collection_tag", "Value": tag_value}]},
         )
 
 
-def get_plaintext_key_calling_dks(encryptedkey, keyencryptionkeyid):
+def get_plaintext_key_calling_dks(encryptedkey, keyencryptionkeyid, keys_map):
     if keys_map.get(encryptedkey):
         key = keys_map[encryptedkey]
     else:
@@ -200,7 +208,7 @@ def call_dks(cek, kek):
     return content["plaintextDataKey"]
 
 
-def read_binary(file_path):
+def read_binary(spark, file_path):
     return spark.sparkContext.binaryFiles(file_path)
 
 
@@ -223,15 +231,10 @@ def persist_json(json_location, values):
 def get_collection(collection_name):
     return (
         collection_name.replace("db.", "", 1)
-        .replace(".", "_")
-        .replace("-", "_")
-        .lower()
+            .replace(".", "_")
+            .replace("-", "_")
+            .lower()
     )
-
-
-def get_published_db_name():
-    published_database_name = "${published_db}"
-    return published_database_name
 
 
 def get_collections(secrets_response):
@@ -243,44 +246,44 @@ def get_collections(secrets_response):
     return collections
 
 
-def create_hive_on_published(json_location, collection_name):
+def create_hive_on_published(spark, json_location, collection_name, published_database_name):
     hive_table_name = get_collection(collection_name)
     src_hive_table = published_database_name + "." + hive_table_name
-    # print(f'src_hive_table ======={hive_table_name}')
     the_logger.info("Creating Hive tables  : " + src_hive_table)
-    src_hive_drop_query = "DROP TABLE IF EXISTS %s" % src_hive_table
+    src_hive_drop_query = f"DROP TABLE IF EXISTS {src_hive_table}"
     src_hive_create_query = (
-        """CREATE EXTERNAL TABLE IF NOT EXISTS %s(val STRING) STORED AS TEXTFILE LOCATION "%s" """
-        % (src_hive_table, json_location)
+        f"""CREATE EXTERNAL TABLE IF NOT EXISTS {src_hive_table}(val STRING) STORED AS TEXTFILE LOCATION "{json_location}" """
     )
     spark.sql(src_hive_drop_query)
     spark.sql(src_hive_create_query)
-    return None
 
-def add_filesize_metric(collection_name, s3_htme_bucket, collection_file_key):
+
+def add_filesize_metric(collection_name, s3_client, s3_htme_bucket, collection_file_key):
     metadata = s3_client.head_object(Bucket=s3_htme_bucket, Key=collection_file_key)
     add_metric("collection_size.csv", collection_name, metadata['ResponseMetadata']['HTTPHeaders']['content-length'])
 
+
 def add_metric(metrics_file, collection_name, value):
-    metrics_path = "/opt/emr/metrics/" + metrics_file
+    metrics_path = f"/opt/emr/metrics/{metrics_file}"
     if not os.path.exists(metrics_path):
         os.mknod(metrics_path)
     with open(metrics_path, "r") as f:
         lines = f.readlines()
     with open(metrics_path, "w") as f:
         for line in lines:
-            if not (line.startswith(get_collection(collection_name))):
+            if not line.startswith(get_collection(collection_name)):
                 f.write(line)
         f.write(get_collection(collection_name) + "," + value + "\n")
+
 
 def get_spark_session():
     spark = (
         SparkSession.builder.master("yarn")
-        .config("spark.metrics.conf", "/opt/emr/metrics/metrics.properties")
-        .config("spark.metrics.namespace", "adg")
-        .appName("spike")
-        .enableHiveSupport()
-        .getOrCreate()
+            .config("spark.metrics.conf", "/opt/emr/metrics/metrics.properties")
+            .config("spark.metrics.namespace", "adg")
+            .appName("spike")
+            .enableHiveSupport()
+            .getOrCreate()
     )
     spark.conf.set("spark.scheduler.mode", "FAIR")
     return spark
@@ -289,7 +292,7 @@ def get_spark_session():
 if __name__ == "__main__":
     spark = get_spark_session()
     run_time_stamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    published_database_name = get_published_db_name()
+    published_database_name = "${published_db}"
     s3_htme_bucket = os.getenv("S3_HTME_BUCKET")
     s3_prefix = "${s3_prefix}"
     s3_publish_bucket = os.getenv("S3_PUBLISH_BUCKET")
@@ -298,7 +301,8 @@ if __name__ == "__main__":
     secrets_collections = get_collections(secrets_response)
     keys_map = {}
     start_time = time.perf_counter()
-    main()
+    main(spark, s3_client, s3_htme_bucket, s3_prefix, secrets_collections, keys_map, run_time_stamp, s3_publish_bucket,
+         published_database_name)
     end_time = time.perf_counter()
     total_time = round(end_time - start_time)
     add_metric("processing_times.csv", "all_collections", str(total_time))
---
+++ steps/test_generate_dataset_from_htme.py
@@ -0,0 +1,111 @@
+import zlib
+
+import boto3
+import generate_dataset_from_htme
+from moto import mock_s3
+
+
+def test_retrieve_secrets(monkeypatch, aws_credentials):
+    class MockSession:
+        class Session:
+            def client(self, service_name):
+                class Client:
+                    def get_secret_value(self, SecretId):
+                        return {"SecretBinary": str.encode("{'collections_all': {'db.core.contract': 'crown'}}")}
+
+                return Client()
+
+    monkeypatch.setattr(boto3, 'session', MockSession)
+    assert generate_dataset_from_htme.retrieve_secrets() == {'collections_all': {'db.core.contract': 'crown'}}
+
+
+def test_get_collections():
+    secret_dict = {'collections_all': {'db.core.contract': 'crown'}}
+    assert generate_dataset_from_htme.get_collections(secret_dict) == {'db.core.contract': 'crown'}
+
+
+@mock_s3
+def test_get_list_keys_for_prefix(monkeypatch, aws_credentials):
+    s3_client = boto3.client('s3')
+    s3_htme_bucket = "test"
+    s3_prefix = "mongo/ucdata"
+    file_name = "db.core.contract.01002.4040.gz.enc"
+    s3_client.create_bucket(Bucket=s3_htme_bucket)
+    s3_client.put_object(Body="test", Bucket=s3_htme_bucket, Key=f'{s3_prefix}/{file_name}')
+
+    assert (
+            generate_dataset_from_htme.get_list_keys_for_prefix(s3_client, s3_htme_bucket, s3_prefix) ==
+            [f"{s3_prefix}/{file_name}"]
+    )
+
+
+def test_group_keys_by_collection():
+    keys = ["mongo/ucdata/db.core.contract.01002.4040.gz.enc",
+            "mongo/ucdata/db.core.accounts.01002.4040.gz.enc"]
+    assert (
+            generate_dataset_from_htme.group_keys_by_collection(keys) ==
+            [{'db.core.contract': ['mongo/ucdata/db.core.contract.01002.4040.gz.enc']},
+             {'db.core.accounts': ['mongo/ucdata/db.core.accounts.01002.4040.gz.enc']}]
+    )
+
+
+def test_get_collections_in_secrets():
+    list_of_dicts = [{'db.core.contract': ['mongo/ucdata/db.core.contract.01002.4040.gz.enc']},
+                     {'db.core.accounts': ['mongo/ucdata/db.core.accounts.01002.4040.gz.enc']}]
+    secrets_collections = {'db.core.contract': 'crown'}
+    expected_list_of_dicts = [{'db.core.contract': ['mongo/ucdata/db.core.contract.01002.4040.gz.enc']}]
+    assert generate_dataset_from_htme.get_collections_in_secrets(list_of_dicts,
+                                                                 secrets_collections) == expected_list_of_dicts
+
+
+@mock_s3
+def test_consolidate_rdd_per_collection(spark, monkeypatch, handle_server, aws_credentials):
+    collection = {'db.core.contract': ['mongo/ucdata/db.core.contract.01002.4040.gz.enc']}
+    secrets_collections = {'db.core.contract': 'crown'}
+    collection_name = "core_contract"
+    s3_htme_bucket = "test"
+    s3_publish_bucket = "target"
+    s3_prefix = "mongo/ucdata"
+    file_name = "db.core.contract.01002.4040.gz.enc"
+    keys_map = {"test_ciphertext": "test_key"}
+    run_time_stamp = "10-10-2000_10-10-10"
+    published_database_name = "test_db"
+    test_data = b'{"name":"abcd"}\n{"name":"xyz"}'
+    target_object_key = f'${{file_location}}/{run_time_stamp}/{collection_name}/{collection_name}.json/part-00000'
+    target_object_tag = {'Key': 'collection_tag', 'Value': 'crown'}
+    s3_client = boto3.client("s3", endpoint_url="http://127.0.0.1:5000")
+
+    s3_client.create_bucket(Bucket=s3_htme_bucket)
+    s3_client.create_bucket(Bucket=s3_publish_bucket)
+    s3_client.put_object(Body=zlib.compress(test_data), Bucket=s3_htme_bucket, Key=f'{s3_prefix}/{file_name}',
+                         Metadata={'iv': '123', 'ciphertext': 'test_ciphertext', 'datakeyencryptionkeyid': '123'})
+    monkeypatch.setattr(generate_dataset_from_htme, 'add_metric', mock_add_metric)
+    monkeypatch.setattr(generate_dataset_from_htme, 'decompress', mock_decompress)
+    monkeypatch.setattr(generate_dataset_from_htme, 'decrypt', mock_decrypt)
+    monkeypatch.setattr(generate_dataset_from_htme, 'call_dks', mock_call_dks)
+    generate_dataset_from_htme.main(spark, s3_client, s3_htme_bucket, s3_prefix, secrets_collections, keys_map,
+                                    run_time_stamp, s3_publish_bucket, published_database_name)
+    generate_dataset_from_htme.consolidate_rdd_per_collection(collection, secrets_collections, s3_client,
+                                                              s3_htme_bucket, spark, keys_map, run_time_stamp,
+                                                              s3_publish_bucket, published_database_name)
+    assert len(s3_client.list_buckets()['Buckets']) == 2
+    assert len(s3_client.list_objects(Bucket=s3_publish_bucket)['Contents']) == 3
+    assert (s3_client.get_object(Bucket=s3_publish_bucket,
+                                 Key=target_object_key)['Body'].read().decode().strip() == test_data.decode())
+    assert s3_client.get_object_tagging(Bucket=s3_publish_bucket, Key=target_object_key)['TagSet'][0] == target_object_tag
+
+
+def mock_decompress(compressed_text):
+    return zlib.decompress(compressed_text)
+
+
+def mock_add_metric(metrics_file, collection_name, value):
+    pass
+
+
+def mock_decrypt(plain_text_key, iv_key, data):
+    return data
+
+
+def mock_call_dks(cek, kek):
+    pass
