988eb7722d8a78c1855613188c82bb7e9a5a7092
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-09-07 12:45:37 +0100

    formatting

--- lambda_manage_mysql_user.tf
+++ lambda_manage_mysql_user.tf
@@ -77,7 +77,7 @@ resource "aws_lambda_function" "manage_mysql_user" {
   publish = false
 
   vpc_config {
-    subnet_ids = data.terraform_remote_state.internal_compute.outputs.pdm_subnet.ids
+    subnet_ids         = data.terraform_remote_state.internal_compute.outputs.pdm_subnet.ids
     security_group_ids = [aws_security_group.adg_common.id]
   }
 
