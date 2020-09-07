540dbe89fb2caf5b04cbffadc7d9922614e63951
Levan K <37900502+TheDogfather17@users.noreply.github.com>
2020-09-07 14:11:16 +0100

    Update lambda_manage_mysql_user.tf
    Co-authored-by: Matthew Burgess <549318+mattburgess@users.noreply.github.com>

--- lambda_manage_mysql_user.tf
+++ lambda_manage_mysql_user.tf
@@ -78,7 +78,7 @@ resource "aws_lambda_function" "manage_mysql_user" {
 
   vpc_config {
     subnet_ids         = data.terraform_remote_state.internal_compute.outputs.pdm_subnet.ids
-    security_group_ids = [aws_security_group.adg_common.id]
+    security_group_ids = [aws_security_group.metastore_rds_user_lambda.id]
   }
 
   timeout                        = 300
