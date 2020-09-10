bb514af44e2d7c7090fb93dece55c4e6a3ee0e72
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-09-10 13:24:53 +0100

    removed unnecessaray  stuff and created database

--- steps.tf
+++ steps.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket_object" "generate_dataset_from_htme_script" {
   content = templatefile("${path.module}/steps/generate_dataset_from_htme.py",
     {
       secret_name        = local.secret_name
-      published_db       = "analytical_dataset_generation"
+      published_db       = local.published_db
       file_location      = "analytical-dataset"
       url                = format("%s/datakey/actions/decrypt", data.terraform_remote_state.crypto.outputs.dks_endpoint[local.environment])
       aws_default_region = "eu-west-2"
@@ -22,7 +22,7 @@ resource "aws_s3_bucket_object" "hive_setup_sh" {
       hive-scripts-path           = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.create-hive-tables.key)
       python_logger               = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.logger.key)
       generate_analytical_dataset = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.generate_dataset_from_htme_script.key)
-      adg_published_db            = "analytical_dataset_generation"
+      published_db                = local.published_db
     }
   )
 }
