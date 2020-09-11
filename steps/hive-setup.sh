f36607712cf39a63d7eaff5edd4881522f0e23f5
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2020-09-11 13:50:19 +0100

    removed legacy code

--- steps/hive-setup.sh
+++ steps/hive-setup.sh
@@ -7,8 +7,7 @@ source /opt/emr/logging.sh
 function log_wrapper_message() {
     log_adg_message "$1" "hive-setup.sh" "$$" "Running as: $USER"
 }
-
-aws s3 cp "${hive-scripts-path}" /opt/emr/.
+aws s3 cp "${python_logger}" /opt/emr/.
 aws s3 cp "${generate_analytical_dataset}" /opt/emr/.
 hive -e "CREATE DATABASE IF NOT EXISTS ${published_db}"
 
