a0a8d70eaa38219ca82ec18846fa31efb6159626
Levan Kirvalidze <levanik@live.com>
2020-09-23 12:40:50 +0100

    modified the create external hive script step syntax

--- steps/create-hive-dynamo-table.sh
+++ steps/create-hive-dynamo-table.sh
@@ -10,14 +10,14 @@ function log_wrapper_message() {
 }
 
 
-log_wrapper_message "Starting hive script"
+log_wrapper_message "Creating external hive table"
 
-hive -e CREATE EXTERNAL TABLE data_pipeline_metadata_hive (Run_Id String, Correlation_Id String, DataProduct String, Status String, Date String)
-STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler'
-TBLPROPERTIES ("dynamodb.table.name"={dynamodb_table_name},
-"dynamodb.column.mapping" = "RunId:Run_Id, Correlation_Id: Correlation_Id, DataProduct: DataProduct, Status:Status, Date:Date",
-"dynamodb.null.serialization" = "true");
-
+hive -e "CREATE EXTERNAL TABLE IF NOT EXISTS data_pipeline_metadata_hive \
+(Correlation_Id STRING, Run_Id BIGINT, DataProduct STRING, Dates STRING, Status STRING) \
+STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
+TBLPROPERTIES ('dynamodb.table.name'='data_pipeline_metadata', \
+'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,Dates:Date,Status:Status', \
+'dynamodb.null.serialization' = 'true');"
 ) >> /var/log/adg/nohup.log 2>&1
 
-log_wrapper_message "Finishing hive script"
+log_wrapper_message "Finished creating external hive table"
