a259a01db5ff103055179e91ccd4d75c855818ec
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-09-30 16:15:17 +0100

    acm-cert-retriever is on $PATH so don't fully qualify it
    acm-cert-retrieve gets installed to /usr/local/bin on AL1 images
    and to /usr/bin on AL2 images. Both of those locations are on
    $PATH so just call it without a preceding path.

--- bootstrap_actions/emr-setup.sh
+++ bootstrap_actions/emr-setup.sh
@@ -78,7 +78,7 @@ EOF
 
 log_wrapper_message "Retrieving the ACM Certificate details"
 
-/usr/local/bin/acm-cert-retriever \
+acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --keystore-path "/opt/emr/keystore.jks" \
@@ -92,7 +92,7 @@ log_wrapper_message "Retrieving the ACM Certificate details"
     --jks-only true >> /var/log/adg/acm-cert-retriever.log 2>&1
 
 
-sudo -E /usr/local/bin/acm-cert-retriever \
+sudo -E acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --private-key-alias "${private_key_alias}" \
