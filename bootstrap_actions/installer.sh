d8115df6e77ebd4d9568bd1fd82a114d8fccd847
Matt Burgess <549318+mattburgess@users.noreply.github.com>
2020-09-30 09:49:53 +0100

    PyCrypto needs the Python development headers in order to compile

--- bootstrap_actions/installer.sh
+++ bootstrap_actions/installer.sh
@@ -27,7 +27,9 @@ fi
 
 sudo -E $PIP install boto3 >> /var/log/adg/install-boto3.log 2>&1
 sudo -E $PIP install requests >> /var/log/adg/install-requests.log 2>&1
+sudo yum install -y python3-devel >> /var/log/adg/install-pycrypto.log 2>&1
 sudo -E $PIP install pycrypto >> /var/log/adg/install-pycrypto.log 2>&1
+sudo yum remove -y python3-devel >> /var/log/adg/install-pycrypto.log 2>&1
 
 log_wrapper_message "Completed the installer.sh step of the EMR Cluster"
 
