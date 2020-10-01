2dabb857fa6110b488abf62719cbe94c4cc5cd59
levankirvalidze <levan.kirvalidze@dmwgroup.co.uk>
2020-10-01 13:41:39 +0100

    creating new sns topic

---
+++ adg_completion_status_sns.tf
@@ -0,0 +1,63 @@
+resource "aws_sns_topic" "adg_completion_status_sns" {
+  name = "adg_completion_status_sns"
+
+  tags = merge(
+  local.common_tags,
+  {
+    "Name" = "adg_completion_status_sns"
+  },
+  )
+}
+
+resource "aws_sns_topic_policy" "adg_completion_status_sns" {
+  arn    = aws_sns_topic.adg_completion_status_sns.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "AdgCompletionStatusSnsTopicPolicy"
+
+  statement {
+    sid = "DefaultPolicy"
+
+    actions = [
+      "SNS:GetTopicAttributes",
+      "SNS:SetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:RemovePermission",
+      "SNS:DeleteTopic",
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:Publish",
+      "SNS:Receive",
+    ]
+
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        local.account[local.environment],
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        "*"]
+    }
+
+    resources = [
+      aws_sns_topic.adg_completion_status_sns.arn,
+    ]
+  }
+}
+
+output "adg_completion_status_sns_topic" {
+  value = {
+    arn  = aws_sns_topic.adg_completion_status_sns.arn
+    name = aws_sns_topic.adg_completion_status_sns.name
+  }
+}
