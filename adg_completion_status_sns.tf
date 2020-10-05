972fbcbb7ebbdbd39dbcd697f40e4c0063e1c857
abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
2020-10-05 11:05:12 +0100

    Removed not needed output

--- adg_completion_status_sns.tf
+++ adg_completion_status_sns.tf
@@ -12,7 +12,3 @@ resource "aws_sns_topic" "adg_completion_status_sns" {
 output "adg_completion_status_sns_topic" {
   value = aws_sns_topic.adg_completion_status_sns
 }
-
-output "adg_completion_status_sns_topic_arn" {
-  value = aws_sns_topic.adg_completion_status_sns.arn
-}
