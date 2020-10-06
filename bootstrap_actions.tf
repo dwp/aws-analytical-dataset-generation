e280755c63a030b0b99347db37d6db0fd2dd8c52
Levan Kirvalidze <levanik@live.com>
2020-10-06 15:12:57 +0100

    matching proxy

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -17,8 +17,8 @@ resource "aws_s3_bucket_object" "emr_setup_sh" {
       S3_LOGGING_SHELL                = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.logging_script.key)
       S3_SEND_SNS_NOTIFICATION        = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.send_notification_script.key)
       aws_default_region              = "eu-west-2"
-      http_proxy                      = data.terraform_remote_state.internal_compute.outputs.internet_proxy.url
-      non_proxied_domains             = local.no_proxy
+      full_proxy                      = data.terraform_remote_state.internal_compute.outputs.internet_proxy.url
+      full_no_proxy                   = local.no_proxy
       acm_cert_arn                    = aws_acm_certificate.analytical-dataset-generator.arn
       private_key_alias               = "private_key"
       truststore_aliases              = join(",", var.truststore_aliases)
