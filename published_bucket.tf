5676d0d979d74bea27b068d87e139ad98826b304
Camilla Scuffi <43463939+camillascf@users.noreply.github.com>
2020-10-13 16:24:29 +0100

    Update published_bucket.tf
    Co-authored-by: Levan K <37900502+TheDogfather17@users.noreply.github.com>

--- published_bucket.tf
+++ published_bucket.tf
@@ -436,7 +436,7 @@ data "aws_iam_policy_document" "pdm_read_pii_and_non_pii" {
     resources = [
       "${aws_s3_bucket.published.arn}/pdm-dataset/*",
       "${aws_s3_bucket.published.arn}/aws-analytical-env-metrics-data/*",
-      data.terraform_remote_state.common.outputs.published_bucket_non_pii.arn
+      data.terraform_remote_state.common.outputs.published_bucket_non_pii.arn,
     ]
 
   }
