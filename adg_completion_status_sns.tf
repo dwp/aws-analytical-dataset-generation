fb96f82faa6372ea6637e63c18c2e196b299f8b2
camillascf <camilla.scuffi [at] dmwgroup.co.uk>
2020-10-13 13:13:11 +0100

    backed file to its latest version

--- adg_completion_status_sns.tf
+++ adg_completion_status_sns.tf
@@ -9,47 +9,6 @@ resource "aws_sns_topic" "adg_completion_status_sns" {
   )
 }
 
-resource "aws_sns_topic_policy" "adg_completion_status_sns" {
-  arn    = aws_sns_topic.adg_completion_status_sns.arn
-  policy = data.aws_iam_policy_document.sns_topic_policy.json
-}
-
-data "aws_iam_policy_document" "sns_topic_policy" {
-  policy_id = "AdgCompletionStatusSnsTopicPolicy"
-
-  statement {
-    sid = "AdgSnsPolicy"
-
-    actions = [
-      "SNS:Get*",
-      "SNS:List*",
-      "SNS:Subscribe",
-      "SNS:Publish",
-    ]
-
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceOwner"
-
-      values = [
-        local.account[local.environment],
-      ]
-    }
-
-    effect = "Allow"
-
-    principals {
-      type = "AWS"
-      identifiers = [
-      "*"]
-    }
-
-    resources = [
-      aws_sns_topic.adg_completion_status_sns.arn,
-    ]
-  }
-}
-
 output "adg_completion_status_sns_topic" {
   value = aws_sns_topic.adg_completion_status_sns
 }
