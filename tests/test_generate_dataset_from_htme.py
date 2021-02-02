a96b1f4400388e3b5bdd1a6bc705d266e98977aa
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-02-02 11:46:08 +0000

    Static spark session methods

--- tests/test_generate_dataset_from_htme.py
+++ tests/test_generate_dataset_from_htme.py
@@ -137,6 +137,9 @@ def test_consolidate_rdd_per_collection_with_one_collection(
         steps.generate_dataset_from_htme, "get_resource", mock_get_dynamodb_resource
     )
     monkeypatch.setattr(
+        steps.generate_dataset_from_htme, "get_s3_resource", s3_resource
+    )
+    monkeypatch.setattr(
         steps.generate_dataset_from_htme, "get_client", s3_client
     )
     generate_dataset_from_htme.main(
@@ -229,6 +232,9 @@ def test_consolidate_rdd_per_collection_with_multiple_collections(
         steps.generate_dataset_from_htme, "get_resource", mock_get_dynamodb_resource
     )
     monkeypatch.setattr(
+        steps.generate_dataset_from_htme, "get_s3_resource", s3_resource
+    )
+    monkeypatch.setattr(
         steps.generate_dataset_from_htme, "get_client", s3_client
     )
     generate_dataset_from_htme.main(
@@ -292,6 +298,9 @@ def test_exception_when_decompression_fails(
         monkeypatch.setattr(steps.generate_dataset_from_htme, "call_dks", mock_call_dks)
         monkeypatch.setattr(
             steps.generate_dataset_from_htme, "get_resource", mock_get_dynamodb_resource
+        )
+        monkeypatch.setattr(
+            steps.generate_dataset_from_htme, "get_s3_resource", s3_resource
         )
         monkeypatch.setattr(
             steps.generate_dataset_from_htme, "get_client", s3_client
