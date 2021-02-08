bfe6a556393d96b011e013188357e4fb49ba981d
abdu-n <57192333+abdu-n@users.noreply.github.com>
2021-02-08 10:22:19 +0000

    DW 5590 Added script for creating mongo latest db (#503)
    * DW 5590 Added script for creating mongo latest db
    
    * DW-5590 fixed typos
    
    Co-authored-by: abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
    Co-authored-by: steveburton4 <Steven.burton@hotmail.co.uk>

---
+++ steps/create-mongo-latest-dbs.sh
@@ -0,0 +1,21 @@
+(
+# Import the logging functions
+source /opt/emr/logging.sh
+
+PROCESSED_BUCKET="${processed_bucket}"
+PUBLISHED_BUCKET="${publish_bucket}"
+
+function log_wrapper_message() {
+    log_adg_message "$${1}" "create-mongo-latest-dbs.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
+}
+
+log_wrapper_message "Starts 'create-mongo-latest-dbs' step"
+
+#create mongo latest databasess
+hive -e "CREATE DATABASE IF NOT EXISTS uc_mongo_latest LOCATION '$PROCESSED_BUCKET/uc_mongo_latest/data';" 
+hive -e "CREATE DATABASE IF NOT EXISTS ucs_latest_unredacted LOCATION '$PUBLISHED_BUCKET/ucs_latest_unredacted/data';"
+hive -e "CREATE DATABASE IF NOT EXISTS ucs_latest_redacted LOCATION  '$PUBLISHED_BUCKET/ucs_latest_redacted/data';"
+
+log_wrapper_message "Completed 'create-mongo-latest-dbs' step of the EMR Cluster"
+
+) >> /var/log/adg/create-mongo-latest-dbs.log 2>&1
