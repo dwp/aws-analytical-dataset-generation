5cf9b491b77abe9cb497a38e7c4803416930af87
abdurrahmannuhu <abdurrahman.nuhu@dwp.gsi.gov.ukgit config --global user.name abdurrahmannuhugit config --global user.email abdurrahman.nuhu@dwp.gsi.gov.uk>
2021-02-08 10:03:05 +0000

    DW-5590 fixed typos

--- steps/create-mongo-latest-dbs.sh
+++ steps/create-mongo-latest-dbs.sh
@@ -9,13 +9,13 @@ function log_wrapper_message() {
     log_adg_message "$${1}" "create-mongo-latest-dbs.sh" "$${PID}" "$${@:2}" "Running as: ,$USER"
 }
 
-log_wrapper_message "Starts create-mongo-latest-dbs step"
+log_wrapper_message "Starts 'create-mongo-latest-dbs' step"
 
 #create mongo latest databasess
 hive -e "CREATE DATABASE IF NOT EXISTS uc_mongo_latest LOCATION '$PROCESSED_BUCKET/uc_mongo_latest/data';" 
 hive -e "CREATE DATABASE IF NOT EXISTS ucs_latest_unredacted LOCATION '$PUBLISHED_BUCKET/ucs_latest_unredacted/data';"
 hive -e "CREATE DATABASE IF NOT EXISTS ucs_latest_redacted LOCATION  '$PUBLISHED_BUCKET/ucs_latest_redacted/data';"
 
-log_wrapper_message "Completed the create-mongo-latest-dbs.sh step of the EMR Cluster"
+log_wrapper_message "Completed 'create-mongo-latest-dbs' step of the EMR Cluster"
 
 ) >> /var/log/adg/create-mongo-latest-dbs.log 2>&1
