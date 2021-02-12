4c076e0f2d5b48051a9e562bd3bc2773522e0001
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-02-12 11:07:59 +0000

    Match PDM
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>

--- bootstrap_actions/emr-setup.sh
+++ bootstrap_actions/emr-setup.sh
@@ -84,7 +84,7 @@ EOF
     
     log_wrapper_message "Retrieving the ACM Certificate details"
     
-    /usr/local/bin/acm-cert-retriever \
+    acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --keystore-path "/opt/emr/keystore.jks" \
@@ -98,7 +98,7 @@ EOF
     --jks-only true >> /var/log/adg/acm-cert-retriever.log 2>&1
     
     
-    sudo -E /usr/local/bin/acm-cert-retriever \
+    sudo -E acm-cert-retriever \
     --acm-cert-arn "${acm_cert_arn}" \
     --acm-key-passphrase "$ACM_KEY_PASSWORD" \
     --private-key-alias "${private_key_alias}" \
