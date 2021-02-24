8146d3b2b7c96a59009bc8fafa184e441c9c7d66
PhanindraVenkata <phanindra.venkata@dwp.gsi.gov.uk>
2021-02-24 12:32:40 +0000

    new logging to mongo-latest

--- local.tf
+++ local.tf
@@ -200,11 +200,11 @@ locals {
   }
 
   mongo_latest_version = {
-    development = "0.0.22"
-    qa          = "0.0.22"
-    integration = "0.0.22"
-    preprod     = "0.0.22"
-    production  = "0.0.22"
+    development = "0.0.24"
+    qa          = "0.0.24"
+    integration = "0.0.24"
+    preprod     = "0.0.24"
+    production  = "0.0.24"
   }
 
   skip_sns_notification_on_adg_completion = {
