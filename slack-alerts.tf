e008f2b126037f291ae140be7bff4c4920c13db3
Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
2021-02-25 11:04:21 +0000

    Fix CW rules and alerts (#569)
    * Fix CW rules and alerts
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * full
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    * High
    
    Signed-off-by: Chris Scott-Thomas <chris.scott-thomas@infinityworks.com>
    
    Co-authored-by: steveburton4 <Steven.burton@hotmail.co.uk>

--- slack-alerts.tf
+++ slack-alerts.tf
@@ -1,5 +1,5 @@
-resource "aws_cloudwatch_event_rule" "adg_terminated_with_errors_rule" {
-  name          = "adg_terminated_with_errors_rule"
+resource "aws_cloudwatch_event_rule" "adg_full_failed" {
+  name          = "adg_full_failed"
   description   = "Sends failed message to slack when adg cluster terminates with errors"
   event_pattern = <<EOF
 {
@@ -21,33 +21,34 @@ resource "aws_cloudwatch_event_rule" "adg_terminated_with_errors_rule" {
 EOF
 }
 
-resource "aws_cloudwatch_metric_alarm" "adg_failed_with_errors" {
-  alarm_name                = "adg_failed_with_errors"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "TriggeredRules"
-  namespace                 = "AWS/Events"
-  period                    = "60"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "This metric monitors cluster termination with errors"
-  insufficient_data_actions = []
-  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
-  dimensions = {
-    RuleName = aws_cloudwatch_event_rule.adg_terminated_with_errors_rule.name
+resource "aws_cloudwatch_event_rule" "adg_full_terminated" {
+  name          = "adg_full_terminated"
+  description   = "Sends failed message to slack when adg cluster terminates by user request"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Cluster State Change"
+  ],
+  "detail": {
+    "state": [
+      "TERMINATED"
+    ],
+    "name": [
+      "analytical-dataset-generator-full"
+    ],
+    "stateChangeReason": [
+      "{\"code\":\"USER_REQUEST\",\"message\":\"User request\"}"
+    ]
   }
-  tags = merge(
-    local.common_tags,
-    {
-      Name              = "adg_failed_with_errors",
-      notification_type = "Error",
-      severity          = "Critical"
-    },
-  )
+}
+EOF
 }
 
-resource "aws_cloudwatch_event_rule" "adg_success" {
-  name          = "adg_success"
+resource "aws_cloudwatch_event_rule" "adg_full_success" {
+  name          = "adg_full_success"
   description   = "checks that all steps complete"
   event_pattern = <<EOF
 {
@@ -72,9 +73,58 @@ resource "aws_cloudwatch_event_rule" "adg_success" {
 EOF
 }
 
+resource "aws_cloudwatch_metric_alarm" "adg_full_failed" {
+  alarm_name                = "adg_full_failed"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors cluster failed with errors"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.adg_full_failed.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "adg_full_failed",
+      notification_type = "Error",
+      severity          = "Critical"
+    },
+  )
+}
 
-resource "aws_cloudwatch_metric_alarm" "adg_success" {
-  alarm_name                = "adg_completed_all_steps"
+resource "aws_cloudwatch_metric_alarm" "adg_full_terminated" {
+  alarm_name                = "adg_full_terminated"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors cluster terminated by user request"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.adg_full_terminated.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "adg_full_terminated",
+      notification_type = "Information",
+      severity          = "High"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "adg_full_success" {
+  alarm_name                = "adg_full_success"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -86,20 +136,20 @@ resource "aws_cloudwatch_metric_alarm" "adg_success" {
   insufficient_data_actions = []
   alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
   dimensions = {
-    RuleName = aws_cloudwatch_event_rule.adg_success.name
+    RuleName = aws_cloudwatch_event_rule.adg_full_success.name
   }
   tags = merge(
     local.common_tags,
     {
-      Name              = "adg_success",
+      Name              = "adg_full_success",
       notification_type = "Information",
       severity          = "Critical"
     },
   )
 }
 
-resource "aws_cloudwatch_event_rule" "adg_incremental_terminated_with_errors_rule" {
-  name          = "adg_incremental_terminated_with_errors_rule"
+resource "aws_cloudwatch_event_rule" "adg_incremental_failed" {
+  name          = "adg_incremental_failed"
   description   = "Sends failed message to slack when adg cluster terminates with errors"
   event_pattern = <<EOF
 {
@@ -121,29 +171,30 @@ resource "aws_cloudwatch_event_rule" "adg_incremental_terminated_with_errors_rul
 EOF
 }
 
-resource "aws_cloudwatch_metric_alarm" "adg_incremental_failed_with_errors" {
-  alarm_name                = "adg_incremental_failed_with_errors"
-  comparison_operator       = "GreaterThanOrEqualToThreshold"
-  evaluation_periods        = "1"
-  metric_name               = "TriggeredRules"
-  namespace                 = "AWS/Events"
-  period                    = "60"
-  statistic                 = "Sum"
-  threshold                 = "1"
-  alarm_description         = "This metric monitors cluster termination with errors"
-  insufficient_data_actions = []
-  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
-  dimensions = {
-    RuleName = aws_cloudwatch_event_rule.adg_incremental_terminated_with_errors_rule.name
+resource "aws_cloudwatch_event_rule" "adg_incremental_terminated" {
+  name          = "adg_incremental_terminated"
+  description   = "Sends failed message to slack when adg cluster terminates by user request"
+  event_pattern = <<EOF
+{
+  "source": [
+    "aws.emr"
+  ],
+  "detail-type": [
+    "EMR Cluster State Change"
+  ],
+  "detail": {
+    "state": [
+      "TERMINATED"
+    ],
+    "name": [
+      "analytical-dataset-generator-incremental"
+    ],
+    "stateChangeReason": [
+      "{\"code\":\"USER_REQUEST\",\"message\":\"User request\"}"
+    ]
   }
-  tags = merge(
-    local.common_tags,
-    {
-      Name              = "adg_incremental_failed_with_errors",
-      notification_type = "Error",
-      severity          = "Critical"
-    },
-  )
+}
+EOF
 }
 
 resource "aws_cloudwatch_event_rule" "adg_incremental_success" {
@@ -172,9 +223,58 @@ resource "aws_cloudwatch_event_rule" "adg_incremental_success" {
 EOF
 }
 
+resource "aws_cloudwatch_metric_alarm" "adg_incremental_failed" {
+  alarm_name                = "adg_incremental_failed"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors cluster failed with errors"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.adg_incremental_failed.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "adg_incremental_failed",
+      notification_type = "Error",
+      severity          = "Critical"
+    },
+  )
+}
+
+resource "aws_cloudwatch_metric_alarm" "adg_incremental_terminated" {
+  alarm_name                = "adg_incremental_terminated"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "TriggeredRules"
+  namespace                 = "AWS/Events"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "This metric monitors cluster terminated by user request"
+  insufficient_data_actions = []
+  alarm_actions             = [data.terraform_remote_state.security-tools.outputs.sns_topic_london_monitoring.arn]
+  dimensions = {
+    RuleName = aws_cloudwatch_event_rule.adg_incremental_terminated.name
+  }
+  tags = merge(
+    local.common_tags,
+    {
+      Name              = "adg_incremental_terminated",
+      notification_type = "Information",
+      severity          = "High"
+    },
+  )
+}
 
 resource "aws_cloudwatch_metric_alarm" "adg_incremental_success" {
-  alarm_name                = "adg_incremental_completed_all_steps"
+  alarm_name                = "adg_incremental_success"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "TriggeredRules"
@@ -191,7 +291,7 @@ resource "aws_cloudwatch_metric_alarm" "adg_incremental_success" {
   tags = merge(
     local.common_tags,
     {
-      Name              = "adg_incremental_success",
+      Name              = "adg_full_success",
       notification_type = "Information",
       severity          = "Critical"
     },
