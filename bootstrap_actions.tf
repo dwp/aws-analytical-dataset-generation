5c53c608ca431133d0d13207c0b207cc481d4dd3
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-01 14:44:33 +0000

    Sort logging where log names where all over the place for some steps, also make hive setup a bootstrap action as it is required for subsequent steps so those steps can't be restarted without that one running

--- bootstrap_actions.tf
+++ bootstrap_actions.tf
@@ -189,3 +189,16 @@ resource "aws_s3_bucket_object" "update_dynamo_sh" {
     }
   )
 }
+
+resource "aws_s3_bucket_object" "hive_setup_sh" {
+  bucket = data.terraform_remote_state.common.outputs.config_bucket.id
+  key    = "component/analytical-dataset-generation/hive-setup.sh"
+  content = templatefile("${path.module}/bootstrap_actions/hive-setup.sh",
+    {
+      python_logger               = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.logger.key)
+      generate_analytical_dataset = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.generate_dataset_from_htme_script.key)
+      python_resume_script        = format("s3://%s/%s", data.terraform_remote_state.common.outputs.config_bucket.id, aws_s3_bucket_object.resume_step.key)
+      published_db                = local.published_db
+    }
+  )
+}
