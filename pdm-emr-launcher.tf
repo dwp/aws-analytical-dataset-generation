99af0df1f39abc461e7107ccde06a176b25bcc74
Sandeep Gopishetty <sandeep.gopishetty@dmwgroup.co.uk>
2021-03-03 10:59:46 +0000

    DW-5271 changed name to pdm emr launcher

--- pdm-emr-launcher.tf
+++ pdm-emr-launcher.tf
@@ -1,4 +1,4 @@
-variable "pdm_emr_late_launcher_zip" {
+variable "pdm_emr_launcher_zip" {
   type = map(string)
   default = {
     base_path = ""
@@ -6,18 +6,18 @@ variable "pdm_emr_late_launcher_zip" {
   }
 }
 
-resource "aws_lambda_function" "pdm_emr_late_launcher" {
-  filename = "${var.pdm_emr_late_launcher_zip["base_path"]}/dataworks-pdm-emr-launcher-${var.pdm_emr_late_launcher_zip["version"]}.zip"
+resource "aws_lambda_function" "pdm_emr_launcher" {
+  filename = "${var.pdm_emr_launcher_zip["base_path"]}/dataworks-pdm-emr-launcher-${var.pdm_emr_launcher_zip["version"]}.zip"
   //  calling this pdm-emr-late-launcher because we already have a function called pdm-emr-launcher, can change this after we are sure that this works
   function_name = local.pdm_lambda_launcher_name
-  role          = aws_iam_role.pdm_emr_late_launcher_lambda_role.arn
+  role          = aws_iam_role.pdm_emr_launcher_lambda_role.arn
   handler       = "event_handler.handler"
   runtime       = "python3.8"
   source_code_hash = filebase64sha256(
     format(
       "%s/dataworks-pdm-emr-launcher-%s.zip",
-      var.pdm_emr_late_launcher_zip["base_path"],
-      var.pdm_emr_late_launcher_zip["version"]
+      var.pdm_emr_launcher_zip["base_path"],
+      var.pdm_emr_launcher_zip["version"]
     )
   )
   publish = false
@@ -33,12 +33,12 @@ resource "aws_lambda_function" "pdm_emr_late_launcher" {
 }
 
 
-resource "aws_iam_role" "pdm_emr_late_launcher_lambda_role" {
-  name               = "pdm_emr_late_launcher_lambda_role"
-  assume_role_policy = data.aws_iam_policy_document.pdm_emr_late_launcher_assume_policy.json
+resource "aws_iam_role" "pdm_emr_launcher_lambda_role" {
+  name               = "pdm_emr_launcher_lambda_role"
+  assume_role_policy = data.aws_iam_policy_document.pdm_emr_launcher_assume_policy.json
 }
 
-data "aws_iam_policy_document" "pdm_emr_late_launcher_assume_policy" {
+data "aws_iam_policy_document" "pdm_emr_launcher_assume_policy" {
   statement {
     sid     = "PDMEMRLauncherLambdaAssumeRolePolicy"
     effect  = "Allow"
@@ -51,7 +51,7 @@ data "aws_iam_policy_document" "pdm_emr_late_launcher_assume_policy" {
   }
 }
 
-data "aws_iam_policy_document" "pdm_emr_late_launcher_scan_dynamo_policy" {
+data "aws_iam_policy_document" "pdm_emr_launcher_scan_dynamo_policy" {
   statement {
     effect = "Allow"
 
@@ -66,7 +66,7 @@ data "aws_iam_policy_document" "pdm_emr_late_launcher_scan_dynamo_policy" {
   }
 }
 
-data "aws_iam_policy_document" "pdm_emr_late_launcher_sns_policy" {
+data "aws_iam_policy_document" "pdm_emr_launcher_sns_policy" {
   statement {
     sid    = "AllowAccessToSNSLauncherTopic"
     effect = "Allow"
@@ -82,31 +82,31 @@ data "aws_iam_policy_document" "pdm_emr_late_launcher_sns_policy" {
 }
 
 
-resource "aws_iam_policy" "pdm_emr_late_launcher_scan_dynamo_policy" {
+resource "aws_iam_policy" "pdm_emr_launcher_scan_dynamo_policy" {
   name        = "PDMEmrLauncherScanDynamoDb"
   description = "Allow PDM EMR launcher to scan pipeline metadata table"
-  policy      = data.aws_iam_policy_document.pdm_emr_late_launcher_scan_dynamo_policy.json
+  policy      = data.aws_iam_policy_document.pdm_emr_launcher_scan_dynamo_policy.json
 }
 
 
-resource "aws_iam_policy" "pdm_emr_late_launcher_sns_policy" {
+resource "aws_iam_policy" "pdm_emr_launcher_sns_policy" {
   name        = "PDMEmrLauncherPublishSNSMessag"
   description = "Allow PDM EMR launcher to publish SNS message"
-  policy      = data.aws_iam_policy_document.pdm_emr_late_launcher_sns_policy.json
+  policy      = data.aws_iam_policy_document.pdm_emr_launcher_sns_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "pdm_emr_late_launcher_policy_execution" {
-  role       = aws_iam_role.pdm_emr_late_launcher_lambda_role.name
+resource "aws_iam_role_policy_attachment" "pdm_emr_launcher_policy_execution" {
+  role       = aws_iam_role.pdm_emr_launcher_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
 
-resource "aws_iam_role_policy_attachment" "pdm_emr_late_launcher_sns_attachment" {
-  role       = aws_iam_role.pdm_emr_late_launcher_lambda_role.name
-  policy_arn = aws_iam_policy.pdm_emr_late_launcher_sns_policy.arn
+resource "aws_iam_role_policy_attachment" "pdm_emr_launcher_sns_attachment" {
+  role       = aws_iam_role.pdm_emr_launcher_lambda_role.name
+  policy_arn = aws_iam_policy.pdm_emr_launcher_sns_policy.arn
 }
 
-resource "aws_iam_role_policy_attachment" "pdm_emr_late_launcher_scan_dynamo_attachment" {
-  role       = aws_iam_role.pdm_emr_late_launcher_lambda_role.name
-  policy_arn = aws_iam_policy.pdm_emr_late_launcher_scan_dynamo_policy.arn
+resource "aws_iam_role_policy_attachment" "pdm_emr_launcher_scan_dynamo_attachment" {
+  role       = aws_iam_role.pdm_emr_launcher_lambda_role.name
+  policy_arn = aws_iam_policy.pdm_emr_launcher_scan_dynamo_policy.arn
 }
 
