82f38f196f24a4db056bd0eb49bb5fe4e7a4bb3c
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-10 17:05:48 +0000

    Send export date

--- tests/test_trigger_pdm.py
+++ tests/test_trigger_pdm.py
@@ -19,8 +19,8 @@ class TestReplayer(unittest.TestCase):
     @mock.patch("steps.create_pdm_trigger.generate_do_not_run_before_date")
     @mock.patch("steps.create_pdm_trigger.get_events_client")
     @mock.patch("steps.create_pdm_trigger.should_step_be_skipped")
-    @mock.patch("steps.create_pdm_trigger.get_export_date")
     @mock.patch("steps.create_pdm_trigger.generate_cut_off_date")
+    @mock.patch("steps.create_pdm_trigger.get_export_date")
     @mock.patch("steps.create_pdm_trigger.get_now")
     def test_create_pdm_trigger(
         self,
