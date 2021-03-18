738970cfd40069773640be84973b8f1ede97d758
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-03-18 15:07:48 +0000

    Double quote lots before the linting comes in and tells us all to do it anyway

--- bootstrap_actions/metadata.sh
+++ bootstrap_actions/metadata.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" http://169.254.169.254/latest/meta-data)
 
-if [[ $RESPONSE == '200' ]]; then
+if [[ "$RESPONSE" == "200" ]]; then
     echo "IMDSv1 Enabled"
     INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
     REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
-    echo $REGION
-    echo $INSTANCE_ID
+    echo "$REGION"
+    echo "$INSTANCE_ID"
     aws ec2 modify-instance-metadata-options \
-    --region $REGION \
-    --instance-id $INSTANCE_ID \
+    --region "$REGION" \
+    --instance-id "$INSTANCE_ID" \
     --http-tokens optional \
     --http-endpoint enabled
 fi
