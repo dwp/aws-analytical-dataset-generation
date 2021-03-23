37aec05cce7e2152ea8eb88841aeaa3ac3a6ba17
steveburton4 <Steven.burton@hotmail.co.uk>
2021-03-23 12:16:24 +0000

    Looks like it needs two policies now as they as scoped down (#631)

--- emr_service_role.tf
+++ emr_service_role.tf
@@ -11,6 +11,37 @@ data "aws_iam_policy_document" "emr_assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "adg_ec2_policy" {
+  statement {
+    sid = "ADGEC2Actions"
+
+    actions = [
+        "ec2:AuthorizeSecurityGroupEgress",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CancelSpotInstanceRequests",
+        "ec2:Create*",
+        "ec2:DeleteLaunchTemplate",
+        "ec2:DeleteNetworkInterface",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteTags",
+        "ec2:Describe*",
+        "ec2:DetachNetworkInterface",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:RequestSpotInstances",
+        "ec2:RevokeSecurityGroupEgress",
+        "ec2:RunInstances",
+        "ec2:TerminateInstances",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+    ]
+
+    effect = "Allow"
+
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "adg_emr_service" {
   name               = "adg_emr_service"
   assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
@@ -25,6 +56,11 @@ resource "aws_iam_role_policy_attachment" "emr_attachment_full" {
 resource "aws_iam_role_policy_attachment" "emr_attachment_service" {
   role       = aws_iam_role.adg_emr_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEMRServicePolicy_v2"
+}
+
+resource "aws_iam_role_policy_attachment" "adg_ec2_policy" {
+  role       = aws_iam_role.adg_emr_service.name
+  policy_arn = aws_iam_policy.adg_ec2_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "adg_emr_service_ebs_cmk" {
