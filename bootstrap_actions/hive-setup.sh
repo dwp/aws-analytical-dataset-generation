9dafe985bb0e2275cd70806b0cd76acb0fb0b8fb
steveburton4 <Steven.burton@hotmail.co.uk>
2021-03-24 18:14:44 +0000

    Do not stop the service (#644)

--- bootstrap_actions/hive-setup.sh
+++ bootstrap_actions/hive-setup.sh
@@ -29,53 +29,36 @@ set -euo pipefail
 
     log_wrapper_message "Generating fair scheduler xml"
 
+    touch /opt/emr/fair-scheduler.xml
 cat > /opt/emr/fair-scheduler.xml <<FAIR_SCHEDULER_CFG
-    <?xml version=”1.0"?>
-    <allocations>
-        <queue name="root">
-            <schedulingPolicy>fair</schedulingPolicy>
-            <aclSubmitApps> </aclSubmitApps>
-            <aclAdministerApps>*</aclAdministerApps>
-            <queue name="queue1">
-                <schedulingPolicy>fair</schedulingPolicy>
-                <aclSubmitApps>*</aclSubmitApps>
-                <aclAdministerApps>*</aclAdministerApps>
-            </queue>
-            <queue name="queue2">
-                <schedulingPolicy>fair</schedulingPolicy>
-                <aclSubmitApps>*</aclSubmitApps>
-                <aclAdministerApps>*</aclAdministerApps>
-            </queue>
-            <queue name="queue3">
-                <schedulingPolicy>fair</schedulingPolicy>
-                <aclSubmitApps>*</aclSubmitApps>
-                <aclAdministerApps>*</aclAdministerApps>
-            </queue>
-        </queue>
-        <defaultQueueSchedulingPolicy>fair</defaultQueueSchedulingPolicy>
-        <queuePlacementPolicy>
-            <rule name="specified" />
-            <rule name="default" queue="root"/>
-        </queuePlacementPolicy>
-    </allocations>
+<allocations>
+    <queue name="root">
+        <schedulingPolicy>fair</schedulingPolicy>
+        <aclSubmitApps> </aclSubmitApps>
+        <aclAdministerApps>*</aclAdministerApps>
+    </queue>
+    <queue name="queue1" type="parent">
+        <schedulingPolicy>fair</schedulingPolicy>
+        <aclSubmitApps>*</aclSubmitApps>
+        <aclAdministerApps>*</aclAdministerApps>
+    </queue>
+    <queue name="queue2" type="parent">
+        <schedulingPolicy>fair</schedulingPolicy>
+        <aclSubmitApps>*</aclSubmitApps>
+        <aclAdministerApps>*</aclAdministerApps>
+    </queue>
+    <queue name="queue3" type="parent">
+        <schedulingPolicy>fair</schedulingPolicy>
+        <aclSubmitApps>*</aclSubmitApps>
+        <aclAdministerApps>*</aclAdministerApps>
+    </queue>
+    <defaultQueueSchedulingPolicy>fair</defaultQueueSchedulingPolicy>
+    <queuePlacementPolicy>
+        <rule name="specified" />
+        <rule name="default" queue="root"/>
+    </queuePlacementPolicy>
+</allocations>
 FAIR_SCHEDULER_CFG
-    
-    log_wrapper_message "Checking if hadoop-yarn-resourcemanager is running"
-
-    if sudo systemctl is-active hadoop-yarn-resourcemanager; then 
-        log_wrapper_message "Stopping hadoop-yarn-resourcemanager"
-        systemctl stop hadoop-yarn-resourcemanager
-        
-        while sudo systemctl is-active hadoop-yarn-resourcemanager; do
-            log_wrapper_message "Waiting for hadoop-yarn-resourcemanager to stop"
-            sleep 1
-        done
-        
-        log_wrapper_message "Starting hadoop-yarn-resourcemanager"
-        if sudo systemctl enable hadoop-yarn-resourcemanager; then 
-            systemctl start hadoop-yarn-resourcemanager
-        fi
-    fi
 
 ) >> /var/log/adg/hive_setup.log 2>&1
 
