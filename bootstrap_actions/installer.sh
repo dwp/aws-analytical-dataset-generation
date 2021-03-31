9c49670bc36a165acf328141b211c5ff923557dd
Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>
2021-03-31 17:07:06 +0100

    Update PIP install
    Signed-off-by: Connor Avery <ConnorAvery@digital.uc.dwp.gov.uk>

--- bootstrap_actions/installer.sh
+++ bootstrap_actions/installer.sh
@@ -20,12 +20,8 @@
     export NO_PROXY="$FULL_NO_PROXY"
 
     log_wrapper_message "Installing boto3 packages"
-    PIP=/usr/bin/pip3
-
-    if [ ! -x $PIP ]; then
-    # EMR <= 5.29.0 doesn't install a /usr/bin/pip3 wrapper
-    PIP=/usr/bin/pip-3.6
-    fi
+    PIP=$(which pip3)
+    log_wrapper_message "Pip is $PIP"
 
     # No sudo needed to write to file for any of the below, so redirect is fine
     #shellcheck disable=SC2024
@@ -39,10 +35,6 @@
         sudo -E $PIP install pycrypto
         sudo yum remove -y python3-devel
     } >> /var/log/adg/install-pycrypto.log 2>&1
-
-    {
-        sudo -E $PIP install requests
-    } >> /var/log/adg/install-python-requirements.log 2>&1
 
     log_wrapper_message "Completed the installer.sh step of the EMR Cluster"
 
