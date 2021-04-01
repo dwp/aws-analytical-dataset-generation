c62b039eae12ac5f3a49ec28629bdf34cfeb82ed
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-04-01 13:15:54 +0100

    Add missing perms for capacity

--- emr_service_role.tf
+++ emr_service_role.tf
@@ -11,6 +11,38 @@ data "aws_iam_policy_document" "emr_assume_role" {
   }
 }
 
+data "aws_iam_policy_document" "emr_capacity_reservations" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+        "ec2:CreateLaunchTemplateVersion"
+    ]
+
+	resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+		"ec2:DescribeCapacityReservations"
+    ]
+
+	resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+		"resource-groups:ListGroupResources"
+    ]
+
+	resources = ["*"]
+  }
+}
+
 resource "aws_iam_role" "adg_emr_service" {
   name               = "adg_emr_service"
   assume_role_policy = data.aws_iam_policy_document.emr_assume_role.json
@@ -27,6 +59,17 @@ resource "aws_iam_role" "adg_emr_service" {
 resource "aws_iam_role_policy_attachment" "emr_attachment_old" {
   role       = aws_iam_role.adg_emr_service.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonElasticMapReduceRole"
+}
+
+resource "aws_iam_policy" "emr_capacity_reservations" {
+  name        = "ADGCapacityReservations"
+  description = "Allow usage of capacity reservations"
+  policy      = data.aws_iam_policy_document.emr_capacity_reservations.json
+}
+
+resource "aws_iam_role_policy_attachment" "emr_capacity_reservations" {
+  role       = aws_iam_role.adg_emr_service.name
+  policy_arn = aws_iam_policy.emr_capacity_reservations.arn
 }
 
 resource "aws_iam_role_policy_attachment" "adg_emr_service_ebs_cmk" {
