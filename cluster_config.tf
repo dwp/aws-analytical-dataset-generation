1a9914f268dc5533596dd15b380fce4fa27a73bf
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-04-01 14:43:37 +0100

    FMT

--- cluster_config.tf
+++ cluster_config.tf
@@ -29,10 +29,14 @@ resource "aws_s3_bucket_object" "instances" {
   key    = "emr/adg/instances.yaml"
   content = templatefile("${path.module}/cluster_config/instances.yaml.tpl",
     {
-      keep_cluster_alive              = local.keep_cluster_alive[local.environment]
-      add_master_sg                   = aws_security_group.adg_common.id
-      add_slave_sg                    = aws_security_group.adg_common.id
-      subnet_id                       = (local.emr_capacity_reservation_arn[local.environment] == "" ? data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id : data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id)
+      keep_cluster_alive = local.keep_cluster_alive[local.environment]
+      add_master_sg      = aws_security_group.adg_common.id
+      add_slave_sg       = aws_security_group.adg_common.id
+      subnet_id = (
+        local.emr_capacity_reservation_arn[local.environment] == "" ?
+        data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id :
+        data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id
+      )
       master_sg                       = aws_security_group.adg_master.id
       slave_sg                        = aws_security_group.adg_slave.id
       service_access_sg               = aws_security_group.adg_emr_service.id
