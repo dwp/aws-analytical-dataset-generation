99e3490059603aa5ab7ad218c6ee9f39323a385c
udaykiranchokkam <udaykiranchokkam@digital.uc.dwp.gov.uk>
2021-04-26 11:05:32 +0100

    modified audit table to add column created_date

--- steps/create-mongo-latest-dbs.sh
+++ steps/create-mongo-latest-dbs.sh
@@ -29,10 +29,10 @@
     DROP TABLE IF EXISTS AUDIT.data_pipeline_metadata_hive; \
     CREATE EXTERNAL TABLE IF NOT EXISTS AUDIT.data_pipeline_metadata_hive (Correlation_Id STRING, Run_Id BIGINT, \
     DataProduct STRING, DateProductRun STRING, Status STRING, CurrentStep STRING, Cluster_Id STRING, \
-    S3_Prefix_Snapshots STRING, S3_Prefix_Analytical_DataSet STRING, Snapshot_Type STRING) \
+    S3_Prefix_Snapshots STRING, S3_Prefix_Analytical_DataSet STRING, Snapshot_Type STRING, Created_Date STRING) \
     STORED BY 'org.apache.hadoop.hive.dynamodb.DynamoDBStorageHandler' \
     TBLPROPERTIES ('dynamodb.table.name'='${dynamodb_table_name}', \
-    'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status,CurrentStep:CurrentStep,Cluster_Id:Cluster_Id,S3_Prefix_Snapshots:S3_Prefix_Snapshots,S3_Prefix_Analytical_DataSet:S3_Prefix_Analytical_DataSet,Snapshot_Type:Snapshot_Type','dynamodb.null.serialization' = 'true');"
+    'dynamodb.column.mapping' = 'Correlation_Id:Correlation_Id,Run_Id:Run_Id,DataProduct:DataProduct,DateProductRun:Date,Status:Status,CurrentStep:CurrentStep,Cluster_Id:Cluster_Id,S3_Prefix_Snapshots:S3_Prefix_Snapshots,S3_Prefix_Analytical_DataSet:S3_Prefix_Analytical_DataSet,Snapshot_Type:Snapshot_Type,Created_Date:Date','dynamodb.null.serialization' = 'true');"
 
     log_wrapper_message "Finished creating external hive table"
 
