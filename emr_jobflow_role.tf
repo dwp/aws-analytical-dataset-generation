9ee3a2d98643f6ad7e92193338f286d78e9814bf
James Jeffrey <james.jeffrey@engineering.digital.dwp.gov.uk>
2021-05-05 08:48:18 +0100

    DW-6076 Remove unecessary service role

--- emr_jobflow_role.tf
+++ emr_jobflow_role.tf
@@ -24,7 +24,7 @@ resource "aws_iam_instance_profile" "analytical_dataset_generator" {
 
 resource "aws_iam_role_policy_attachment" "ec2_for_ssm_attachment" {
   role       = aws_iam_role.analytical_dataset_generator.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonSSMManagedInstanceCore"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_role_policy_attachment" "amazon_ssm_managed_instance_core" {
