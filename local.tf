fb3523c5c9ee4731a84d8f090acec08dc80faf61
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-13 20:11:54 +0100

    Mongo latest v63

--- local.tf
+++ local.tf
@@ -209,11 +209,11 @@ locals {
   }
 
   mongo_latest_version = {
-    development = "0.0.62"
-    qa          = "0.0.62"
-    integration = "0.0.62"
-    preprod     = "0.0.62"
-    production  = "0.0.62"
+    development = "0.0.63"
+    qa          = "0.0.63"
+    integration = "0.0.63"
+    preprod     = "0.0.63"
+    production  = "0.0.63"
   }
 
   skip_pdm_trigger_on_adg_completion = {
