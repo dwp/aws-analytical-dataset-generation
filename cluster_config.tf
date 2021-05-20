ac8634c1683958470ef7f51902e7b52c97a79b91
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-05-20 16:31:53 +0100

    fix var

--- cluster_config.tf
+++ cluster_config.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket_object" "instances" {
       add_master_sg      = aws_security_group.adg_common.id
       add_slave_sg       = aws_security_group.adg_common.id
       subnet_id = (
-        local.emr_capacity_reservation_preference[local.environment] == true ?
+        local.use_capacity_reservation[local.environment] == true ?
         data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, data.terraform_remote_state.common.outputs.ec2_capacity_reservations.emr_m5_16_x_large_2a.availability_zone)].id :
         data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets[index(data.terraform_remote_state.internal_compute.outputs.adg_subnet_new.subnets.*.availability_zone, local.emr_subnet_non_capacity_reserved_environments)].id
       )
