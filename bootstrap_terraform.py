9936b938343f121d521f31b57c2dbc42cc292527
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-06-11 10:20:08 +0100

    Use secrets manager

--- bootstrap_terraform.py
+++ bootstrap_terraform.py
@@ -6,38 +6,42 @@ import jinja2
 import os
 import sys
 import yaml
+import json
 
 
 def main():
-    if "AWS_PROFILE" in os.environ:
-        boto3.setup_default_session(profile_name=os.environ["AWS_PROFILE"])
-    if "AWS_REGION" in os.environ:
-        ssm = boto3.client("ssm", region_name=os.environ["AWS_REGION"])
+    if 'AWS_PROFILE' in os.environ:
+        boto3.setup_default_session(profile_name=os.environ['AWS_PROFILE'])
+    if 'AWS_REGION' in os.environ:
+        secrets_manager = boto3.client(
+            'secretsmanager', region_name=os.environ['AWS_REGION'])
     else:
-        ssm = boto3.client("ssm")
+        secrets_manager = boto3.client('secretsmanager')
 
     try:
-        parameter = ssm.get_parameter(
-            Name="terraform_bootstrap_config", WithDecryption=False
-        )
+        response = secrets_manager.get_secret_value(
+            SecretId="/concourse/dataworks/terraform")
     except botocore.exceptions.ClientError as e:
         error_message = e.response["Error"]["Message"]
         if "The security token included in the request is invalid" in error_message:
-            print(
-                "ERROR: Invalid security token used when calling AWS SSM. Have you run `aws-sts` recently?"
-            )
+            print("ERROR: Invalid security token used when calling AWS Secrets Manager. Have you run `aws-sts` recently?")
         else:
-            print("ERROR: Problem calling AWS SSM: {}".format(error_message))
+            print("ERROR: Problem calling AWS Secrets Manager: {}".format(
+                error_message))
         sys.exit(1)
 
-    config_data = yaml.load(parameter["Parameter"]["Value"], Loader=yaml.FullLoader)
-    with open("terraform.tf.j2") as in_template:
+    config_data = yaml.load(
+        response['SecretBinary'], Loader=yaml.FullLoader)
+    config_data['terraform'] = json.loads(
+        response['SecretBinary'])["terraform"]
+
+    with open('terraform.tf.j2') as in_template:
         template = jinja2.Template(in_template.read())
-    with open("terraform.tf", "w+") as terraform_tf:
+    with open('terraform.tf', 'w+') as terraform_tf:
         terraform_tf.write(template.render(config_data))
-    with open("terraform.tfvars.j2") as in_template:
+    with open('terraform.tfvars.j2') as in_template:
         template = jinja2.Template(in_template.read())
-    with open("terraform.tfvars", "w") as terraform_tfvars:
+    with open('terraform.tfvars', 'w+') as terraform_tfvars:
         terraform_tfvars.write(template.render(config_data))
     print("Terraform config successfully created")
 
