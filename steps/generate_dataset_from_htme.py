39b1050e042745534704ac95b1649fe2781c2ee4
udaychokkam <udayjavauk@gmail.com>
2021-07-23 11:22:22 +0100

    drop first as well

--- steps/generate_dataset_from_htme.py
+++ steps/generate_dataset_from_htme.py
@@ -586,6 +586,7 @@ def create_audit_log_raw_managed_table(spark, verified_database_name, date_hyphe
         src_external_hive_insert_query = f"""INSERT OVERWRITE TABLE {src_managed_hive_table} SELECT * FROM {src_external_hive_table}"""
         src_externl_hive_drop_query = f"""DROP TABLE IF EXISTS {src_external_hive_table}"""
 
+        spark.sql(src_externl_hive_drop_query)
         spark.sql(src_external_hive_create_query)
         spark.sql(src_external_hive_alter_query)
         spark.sql(src_external_hive_insert_query)
