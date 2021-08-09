023fa5597930e8a63fb393ff04886f1b2e47ab26
stevenburton <stevenburton@digital.uc.dwp.gov.uk>
2021-08-09 11:31:03 +0100

    Use hashes

--- tests/equality_external_table.sql
+++ tests/equality_external_table.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS ${hivevar:equality_database}.equality_${hivevar:date_underscore};
-CREATE EXTERNAL TABLE ${hivevar:equality_database}.equality_${hivevar:date_underscore} (
+DROP TABLE IF EXISTS #{hivevar:equality_database}.equality_#{hivevar:date_underscore};
+CREATE EXTERNAL TABLE #{hivevar:equality_database}.equality_#{hivevar:date_underscore} (
      `message`STRUCT<
      `type`:STRING
      ,`claimantId`:STRING
@@ -8,13 +8,13 @@ CREATE EXTERNAL TABLE ${hivevar:equality_database}.equality_${hivevar:date_under
      ,`sexualOrientation`:STRING
      ,`religion`:STRING
      ,`maritalStatus`:STRING>)
-ROW FORMAT SERDE '${hivevar:serde}'
+ROW FORMAT SERDE '#{hivevar:serde}'
 WITH SERDEPROPERTIES ("ignore.malformed.json" = "true")
 STORED AS TEXTFILE
 LOCATION '#{hivevar:data_location}';
 
 
-INSERT INTO TABLE ${hivevar:equality_database}.equality_managed
+INSERT INTO TABLE #{hivevar:equality_database}.equality_managed
 SELECT
      `message`.`type` as `type`
      ,`message`.`claimantId` as `claimantId`
@@ -24,4 +24,4 @@ SELECT
      ,`message`.`religion`  as `religion`
      ,`message`.`maritalStatus` as `maritalStatus`
      ,CURRENT_DATE as load_date
-FROM ${hivevar:equality_database}.equality_${hivevar:date_underscore};
+FROM #{hivevar:equality_database}.equality_#{hivevar:date_underscore};
