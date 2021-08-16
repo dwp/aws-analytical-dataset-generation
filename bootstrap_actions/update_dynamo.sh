264b85798b7ba5c6d7b3d15e8da07a45f002cceb
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2021-08-16 10:59:22 +0100

    fix for update_dynamo.sh to handle null json values

--- bootstrap_actions/update_dynamo.sh
+++ bootstrap_actions/update_dynamo.sh
@@ -133,12 +133,16 @@
           log_wrapper_message "All steps completed. Final step Name: $CURRENT_STEP, Step status: $state"
           exit 0
         fi
-        if [[ "$PREVIOUS_STATE" != "$state" ]] && [[ "$PREVIOUS_STEP" != "$CURRENT_STEP" ]]; then
-          dynamo_update_item "$CURRENT_STEP" "NOT_SET" "NOT_SET"
-          log_wrapper_message "Successful step. Last step name: $PREVIOUS_STEP, Last step status: $PREVIOUS_STATE, Current step name: $CURRENT_STEP, Current step status: $state"
-          processed_files+=( "$i" )
+        if [[ "$state" == 'null' ]] || [[ "$state" == "" ]]; then # handle null json values
+          sleep 1
         else
-          sleep 0.2
+          if [[ "$PREVIOUS_STATE" != "$state" ]] && [[ "$PREVIOUS_STEP" != "$CURRENT_STEP" ]]; then
+            dynamo_update_item "$CURRENT_STEP" "NOT_SET" "NOT_SET"
+            log_wrapper_message "Successful step. Last step name: $PREVIOUS_STEP, Last step status: $PREVIOUS_STATE, Current step name: $CURRENT_STEP, Current step status: $state"
+            processed_files+=( "$i" )
+          else
+            sleep 0.2
+          fi
         fi
         PREVIOUS_STATE="$state"
         PREVIOUS_STEP="$CURRENT_STEP"
