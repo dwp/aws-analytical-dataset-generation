2bde37bc97bd4e6f113513520d092c3c8e00de71
jon.melia@cloudadept.co.uk <jonathan.melia@engineering.digital.dwp.gov.uk>
2022-10-03 11:49:18 +0100

    Adding sensitive=true flag to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,4 +3,6 @@ output "private_dns" {
     adg_service_discovery_dns = aws_service_discovery_private_dns_namespace.adg_services
     adg_service_discovery     = aws_service_discovery_service.adg_services
   }
+
+  sensitive   = true
 }
