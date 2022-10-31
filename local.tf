ee84afcb9cde6396bedad074ac4ad232d31695b3
jonnym <jon.melia@cloudadept.co.uk>
2022-10-31 10:46:15 +0000

    Dw 9417 (#868)
    * Reverting DW-9417 instance/resource sizes due to ADG failure
    
    * Amended preprod executor instances
    
    * Amended preprod executor instances
    
    Co-authored-by: Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>

--- local.tf
+++ local.tf
@@ -387,7 +387,7 @@ locals {
     development = 5
     qa          = 5
     integration = 5
-    preprod     = 42
+    preprod     = 37
     production  = 10 # Doesn't need as much as executors
   }
 
