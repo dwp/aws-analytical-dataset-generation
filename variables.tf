2e622f78a76ccb8aa8373308a35a904e324e2ff8
Jonathan Melia <jonathan.melia@engineering.digital.dwp.gov.uk>
2022-11-04 09:28:39 +0000

    reverting prod changes again

--- variables.tf
+++ variables.tf
@@ -57,7 +57,7 @@ variable "emr_instance_type_master" {
     qa          = "m5.4xlarge"
     integration = "m5.4xlarge"
     preprod     = "m5.4xlarge"
-    production  = "m5.4xlarge"
+    production  = "m5.16xlarge"
   }
 }
 
@@ -67,7 +67,7 @@ variable "emr_instance_type_core_one" {
     qa          = "m5.4xlarge"
     integration = "m5.4xlarge"
     preprod     = "r5.4xlarge"
-    production  = "r5.4xlarge"
+    production  = "m5.16xlarge"
   }
 }
 
