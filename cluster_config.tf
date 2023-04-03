5493ea1051267760866060288542476176d562c1
rbryett <13303701+rbryett@users.noreply.github.com>
2023-04-03 12:24:55 +0100

    DTWRKS-986 propagate HCS tagging to EMR cluster (#890)
    Co-authored-by: rorybryett <rory.bryett@dwp.gov.uk>

--- cluster_config.tf
+++ cluster_config.tf
@@ -13,13 +13,18 @@ resource "aws_s3_bucket_object" "cluster" {
   key    = "emr/adg/cluster.yaml"
   content = templatefile("${path.module}/cluster_config/cluster.yaml.tpl",
     {
-      s3_log_bucket          = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
-      s3_log_prefix          = local.s3_log_prefix
-      ami_id                 = var.emr_ami_id
-      service_role           = aws_iam_role.adg_emr_service.arn
-      instance_profile       = aws_iam_instance_profile.analytical_dataset_generator.arn
-      security_configuration = aws_emr_security_configuration.ebs_emrfs_em.id
-      emr_release            = var.emr_release[local.environment]
+      s3_log_bucket              = data.terraform_remote_state.security-tools.outputs.logstore_bucket.id
+      s3_log_prefix              = local.s3_log_prefix
+      ami_id                     = var.emr_ami_id
+      service_role               = aws_iam_role.adg_emr_service.arn
+      instance_profile           = aws_iam_instance_profile.analytical_dataset_generator.arn
+      security_configuration     = aws_emr_security_configuration.ebs_emrfs_em.id
+      emr_release                = var.emr_release[local.environment]
+      dwx_environment_tag_value  = local.environment
+      application_tag_value      = data.aws_default_tags.provider_tags.tags.Application
+      function_tag_value         = data.aws_default_tags.provider_tags.tags.Function
+      business_project_tag_value = data.aws_default_tags.provider_tags.tags.Business-Project
+      environment_tag_value      = data.aws_default_tags.provider_tags.tags.Environment
     }
   )
 }
